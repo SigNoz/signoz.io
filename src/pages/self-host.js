f28773daaec0045052e6f664f17dc0f6aef51a58
Akshay Bharadva <akshaybharadva19@gmail.com>
2023-03-29 20:48:31 +0530

    created self-host page #858

---
+++ src/pages/self-host.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+
+function selfhost() {
+  const [email,setEmail] = useState("");
+  const [name,setName] = useState("");
+  const [company,setCompany] = useState("");
+
+  return (
+    <Layout title="Self Host">
+      <section className="selfhost">
+        <div
+          className="container"
+          style={{ marginTop: "4rem", marginBottom: "4rem" }}
+        >
+          <div className="row">
+            <div className="col col--6">
+              <div className="card-demo margin--md">
+                <div className="card no-color">
+                  <div className="card__body">
+                    <p className="" style={{fontWeight: 'bold', fontSize: '42px'}}>SigNoz <span className="highlight"> Enterprise</span></p>
+                    <ul className="dashed">
+                      <li>Managed Self-Hosted SigNoz in your premise or cloud</li>
+                      <li>Single Sign-On</li>
+                      <li>SAML and LDAP support</li>
+                      <li>AWS Private Link</li>
+                      <li>Support for Dashboard configuration from expert engineers</li>
+                      <li>Support plan with SLAs</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col col--6">
+              <div className="card-demo margin--md">
+                <div className="card">
+                  <div className="card__body">
+                    <p className="text-center form-title">Get more info on SigNoz Enterprise</p>
+                    <input
+                      type="text"
+                      name="email"
+                      placeholder={"Email*"}
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required={true}
+                    />
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder={"Name"}
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                      required={true}
+                    />
+                    <input
+                      type="text"
+                      name="company"
+                      placeholder={"Company"}
+                      value={company}
+                      onChange={(e) => setCompany(e.target.value)}
+                      required={true}
+                    />
+                    <button className="submit-btn" type="button">Submit</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+export default selfhost;
