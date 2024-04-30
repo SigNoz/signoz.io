6c8e1b99da430bfd8af49b18dde02f671c771495
CheetoDa <Chitgupta24@gmail.com>
2024-04-30 19:54:25 +0530

    fix: fixed sidebar for alerts

---
+++ docs/product-features/alert-management.md
@@ -0,0 +1,8 @@
+---
+id: alert-management
+title: Alert Management
+sidebar_label: Alert Management
+---
+
+
+Please refer to this documentation [here](../../alerts/)
--- sidebars.js
+++ sidebars.js
@@ -83,36 +83,41 @@ module.exports = {
           label: 'Saved View',
         },
         {
-          type: 'category',
-          label: 'Alerts',
-          items: [
-            {
-              type: 'doc',
-              id: 'product-features/alerts/alerts-notification-channel',
-              label: 'Notification Channel',
-            },
-            {
-              type: 'doc',
-              id: 'product-features/alerts/metrics-based-alerts',
-              label: 'Metrics based Alert',
-            },
-            {
-              type: 'doc',
-              id: 'product-features/alerts/log-based-alerts',
-              label: 'Log based Alert',
-            },
-            {
-              type: 'doc',
-              id: 'product-features/alerts/trace-based-alerts',
-              label: 'Trace based Alert',
-            },
-            {
-              type: 'doc',
-              id: 'product-features/alerts/exceptions-based-alerts',
-              label: 'Exceptions based Alert',
-            },
-          ],
+          type: 'doc',
+          id: 'product-features/alert-management',
+          label: 'Alert Management',
         },
+        // {
+        //   type: 'category',
+        //   label: 'Alerts',
+        //   items: [
+        //     // {
+        //     //   type: 'doc',
+        //     //   id: 'product-features/alerts/alerts-notification-channel',
+        //     //   label: 'Notification Channel',
+        //     // },
+        //     // {
+        //     //   type: 'doc',
+        //     //   id: 'product-features/alerts/metrics-based-alerts',
+        //     //   label: 'Metrics based Alert',
+        //     // },
+        //     // {
+        //     //   type: 'doc',
+        //     //   id: 'product-features/alerts/log-based-alerts',
+        //     //   label: 'Log based Alert',
+        //     // },
+        //     // {
+        //     //   type: 'doc',
+        //     //   id: 'product-features/alerts/trace-based-alerts',
+        //     //   label: 'Trace based Alert',
+        //     // },
+        //     // {
+        //     //   type: 'doc',
+        //     //   id: 'product-features/alerts/exceptions-based-alerts',
+        //     //   label: 'Exceptions based Alert',
+        //     // },
+        //   ],
+        // },
       ]
     },
     {
@@ -500,12 +505,23 @@ module.exports = {
     {
       label: "Alerts",
       type: "category",
+      link: {
+          type: "generated-index",
+          title: "Alert Management in SigNoz",
+          description: 'This documentation helps you in understanding the Alerts feature in SigNoz and how you can create different types of alerts.',
+          slug: '/alerts',
+      },
       items: [
         {
           type: 'doc',
           id: 'userguide/alerts-management',
-          label: 'Set Alert Rules',
+          label: 'Alert Management',
         },
+        // {
+        //   type: 'doc',
+        //   id: 'product-features/alerts/alerts-notification-channel',
+        //   label: 'Notification Channel',
+        // },
         {
           label: "Setup Alerts Notification",
           type: "category",
@@ -513,6 +529,9 @@ module.exports = {
             type: "generated-index",
             title: "Setup Alerts Notifications Channel",
             description: 'You can setup notification channel for sending the generated alerts to other applications. Currently, the following channels are supported.',
+            slug: '/setup-alerts-notification',
+            // type: "doc",
+            // id: "product-features/alerts/alerts-notification-channel",
           },
           items: [
             {
@@ -540,7 +559,32 @@ module.exports = {
               id: 'alerts-management/notification-channel/ms-teams',
               label: 'MS Teams',
             },
+            {
+              type: 'doc',
+              id: 'alerts-management/notification-channel/email',
+              label: 'Email',
+            },
           ],
+        },
+        {
+          type: 'doc',
+          id: 'alerts-management/metrics-based-alerts',
+          label: 'Metrics based Alert',
+        },
+        {
+          type: 'doc',
+          id: 'alerts-management/log-based-alerts',
+          label: 'Log based Alert',
+        },
+        {
+          type: 'doc',
+          id: 'alerts-management/trace-based-alerts',
+          label: 'Trace based Alert',
+        },
+        {
+          type: 'doc',
+          id: 'alerts-management/exceptions-based-alerts',
+          label: 'Exceptions based Alert',
         },
       ],
     },
