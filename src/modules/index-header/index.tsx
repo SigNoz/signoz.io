a020800349b94c92c05a56d85fe396a95d8b4eb1
Yunus A M <myounis.ar@live.com>
2024-05-10 23:28:00 +0530

    update landing page video

--- src/modules/index-header/index.tsx
+++ src/modules/index-header/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Player from "@vimeo/player";
 import SubHeading from "../../components/ui/SubHeading";
 import Hero from "../../components/ui/Hero";
 import Button from "../../components/ui/Button";
@@ -6,8 +7,6 @@ import ReactModal from "react-modal";
 import { LiteYoutubeEmbed } from "react-lite-yt-embed";
 
 export const Header = () => {
-  const [showVideo, setShowVideo] = useState(false);
-
   return (
     <header className="my-16">
       <div className="flex flex-col items-center mb-5 text-center px-5">
@@ -17,9 +16,9 @@ export const Header = () => {
           Metrics, and Logs in a single pane
         </Hero>
         <SubHeading>
-          A single tool for all your observability 
-          needs - APM, logs, metrics, exceptions, <br className="hidden lg:inline" /> alerts, and dashboards powered
-          by a powerful query builder. 
+          A single tool for all your observability needs - APM, logs, metrics,
+          exceptions, <br className="hidden lg:inline" /> alerts, and dashboards
+          powered by a powerful query builder.
         </SubHeading>
       </div>
       <div className="flex flex-col md:flex-row gap-5 justify-center mb-12 mx-5">
@@ -42,40 +41,20 @@ export const Header = () => {
         </Button>
       </div>
       <div className="container">
-        <div className="flex justify-center items-center relative after:-z-[2] after:absolute after:content-[''] after:w-[300px] after:h-[300px] lg:after:w-[600px] lg:after:h-[600px] after:bg-primary-500 after:rounded-full after:opacity-50 after:blur-3xl">
-          <div className="p-2 rounded-lg flex justify-center items-center hero-figure max-w-4xl">
-            <img
-              className="rounded-lg"
-              src="/img/landing/signoz-landing-snap.webp"
-            />
-            <div
-              onClick={() => setShowVideo(true)}
-              className="cursor-pointer play-container w-16 h-16 md:w-24 md:h-24 rounded-full bg-primary-500 flex justify-center items-center focus-visible:outline-none"
-            >
-              <img
-                src="/img/landing/play-icon.webp"
-                className="w-6 h-6 md:w-10 md:h-10"
-              />
+        <div className="w-100 relative mx-auto">
+          <div className="hero-figure p-3 rounded-lg">
+            <div className="embed-container max-w-4xl">
+              <iframe
+                src="https://player.vimeo.com/video/944340217"
+                frameborder="0"
+                webkitAllowFullScreen
+                mozallowfullscreen
+                allowFullScreen
+              ></iframe>
             </div>
           </div>
         </div>
       </div>
-      <ReactModal
-        isOpen={showVideo}
-        contentLabel=""
-        onRequestClose={() => setShowVideo(false)}
-        className="Modal play-video-container"
-        overlayClassName="Overlay"
-      >
-        <div>
-          <LiteYoutubeEmbed
-            id={"jD36hjfL1x0"}
-            defaultPlay={true}
-            mute={false}
-            params={{ autoplay: "true", mute: "false" }}
-          />
-        </div>
-      </ReactModal>
     </header>
   );
 };
