e2aa3f265b377716006764e516b1f6b07b888228
Yunus A M <myounis.ar@live.com>
2024-05-14 08:47:02 +0530

    feat: use vimeo player instance to add custom icon and control video

--- src/modules/index-header/index.tsx
+++ src/modules/index-header/index.tsx
@@ -6,6 +6,47 @@ import Button from "../../components/ui/Button";
 import ReactModal from "react-modal";
 import { LiteYoutubeEmbed } from "react-lite-yt-embed";
 
+const VimeoPlayer = ({ videoId }) => {
+  const playerRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  let player = null;
+
+  useEffect(() => {
+    player = new Player(playerRef.current, {
+      url: `https://vimeo.com/${videoId}`,
+    });
+
+    return () => {
+      player.off("play");
+    };
+  }, [videoId]);
+
+  const handlePlay = () => {
+    if (!isPlaying) {
+      player.play();
+      setIsPlaying(true);
+    }
+  };
+
+  return (
+    <div>
+      <div ref={playerRef} />
+      {!isPlaying && (
+        <div onClick={handlePlay}>
+          <div className="cursor-pointer play-container w-16 h-16 md:w-24 md:h-24 rounded-full bg-primary-500 flex justify-center items-center focus-visible:outline-none">
+            <img
+              src="/img/landing/play-icon.webp"
+              className="w-6 h-6 md:w-10 md:h-10"
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default VimeoPlayer;
+
 export const Header = () => {
   return (
     <header className="my-8">
@@ -44,14 +85,7 @@ export const Header = () => {
         <div className="w-100 mx-auto">
           <div className="product-explainer-video hero-figure p-3 rounded-lg">
             <div className="embed-container">
-              <iframe
-                className="product-explainer-video-frame"
-                src="https://player.vimeo.com/video/944340217"
-                frameborder="0"
-                webkitAllowFullScreen
-                mozallowfullscreen
-                allowFullScreen
-              ></iframe>
+              <VimeoPlayer videoId="944340217" />
             </div>
           </div>
         </div>
