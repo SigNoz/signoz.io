fd89fbf51949a2eb2b03db6775d5fd8d696bbf68
Priyansh Khodiyar <priyanshkhodiyar.27@gmail.com>
2024-05-16 21:25:08 +0530

    added some component

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -7,6 +7,10 @@ import CustomLink from './Link'
 import TableWrapper from './TableWrapper'
 import YouTube from './VideoPlayer/VideoPlayer'
 import GetStartedSigNoz from '../docs/shared/get-started-signoz'
+import SignUps from '../docs/shared/sign-ups'
+import LogsPerf from '../docs/shared/logs-perf-cta'
+import VersionPin from '../docs/shared/nodejs-version-pin'
+import VersionPinNestJs from '../docs/shared/nestjs-version-pin'
 
 export const components: MDXComponents = {
   Image,
@@ -17,4 +21,8 @@ export const components: MDXComponents = {
   BlogNewsletterForm,
   YouTube,
   GetStartedSigNoz,
+  SignUps,
+  LogsPerf,
+  VersionPin,
+  VersionPinNestJs
 }
