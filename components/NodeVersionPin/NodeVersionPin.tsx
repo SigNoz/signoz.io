5083166d3d11f4fb88d2906ca565c1c6b97048d9
Priyansh Khodiyar <priyanshkhodiyar.27@gmail.com>
2024-05-25 15:09:43 +0530

    formatting fixes in docs

--- components/NodeVersionPin/NodeVersionPin.tsx
+++ components/NodeVersionPin/NodeVersionPin.tsx
@@ -2,7 +2,7 @@ import MarkdownRenderer from '@/components/ReactMarkdown'
 import React from 'react'
 
 const markdownContent = `
-<Admonition type="note" title="Note">
+<Admonition">
 If you run into any error, you might want to use these pinned versions of OpenTelemetry libraries used in this <a href = "https://github.com/SigNoz/sample-nodejs-app/blob/master/package.json" rel="noopener noreferrer nofollow" target="_blank">GitHub repo</a>.
 </Admonition>
 `
