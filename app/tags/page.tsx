d39e7dcddb2801d2a20352046f5594fdb330e296
Yunus A M <myounis.ar@live.com>
2024-05-27 13:28:56 +0530

    feat: remove padding left in ul for listwithtags component

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -12,12 +12,12 @@ export default async function Page() {
   const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
   return (
     <div className="container mx-auto">
-      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
-        {/* <div className="space-x-2 pb-8 pt-6 md:space-y-5">
+      <div className="mb-24 flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+        <div className="space-x-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
             Tags
           </h1>
-        </div> */}
+        </div>
         <div className="flex flex-wrap justify-center text-center">
           {tagKeys.length === 0 && 'No tags found.'}
           {sortedTags.map((t) => {
