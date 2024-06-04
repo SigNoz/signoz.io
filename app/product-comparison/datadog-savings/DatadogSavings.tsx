300f7f30af6daf3fe73b6db7e4e97563bdd184e1
Yunus A M <myounis.ar@live.com>
2024-06-04 14:53:07 +0530

    feat: update metadata for pricing page

---
+++ app/product-comparison/datadog-savings/DatadogSavings.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React from 'react'
+import { HubspotProvider } from '@aaronhayes/react-use-hubspot-form'
+import MigrateSaving from '../../../components/comparison/migrate-saving'
+
+const DATA = {
+  TITLE: 'SigNoz vs Datadog Cost Savings',
+  DESC: 'Please provide your contact info and we will reach out to you. We will understand your requirements and identify ways to reduce your observability costs.',
+  PORTAL_ID: '22308423',
+  FORM_ID: 'f33f929c-a9ec-4fde-b805-f54265078d19',
+}
+
+function DatadogSaving() {
+  return (
+    <div title="Datadog Saving">
+      <HubspotProvider>
+        <MigrateSaving data={DATA} />
+      </HubspotProvider>
+    </div>
+  )
+}
+export default DatadogSaving
--- app/product-comparison/datadog-savings/page.tsx
+++ app/product-comparison/datadog-savings/page.tsx
@@ -1,23 +1,11 @@
-'use client'
+import DatadogSaving from './DatadogSavings'
 
-import React from 'react'
-import { HubspotProvider } from '@aaronhayes/react-use-hubspot-form'
-import MigrateSaving from '../../../components/comparison/migrate-saving'
+import { Metadata } from 'next'
 
-const DATA = {
-  TITLE: 'SigNoz vs Datadog Cost Savings',
-  DESC: 'Please provide your contact info and we will reach out to you. We will understand your requirements and identify ways to reduce your observability costs.',
-  PORTAL_ID: '22308423',
-  FORM_ID: 'f33f929c-a9ec-4fde-b805-f54265078d19',
+export const metadata: Metadata = {
+  title: 'SigNoz vs Datadog Cost Savings',
 }
 
-function DatadogSaving() {
-  return (
-    <div title="Datadog Saving">
-      <HubspotProvider>
-        <MigrateSaving data={DATA} />
-      </HubspotProvider>
-    </div>
-  )
+export default function DatadogSavingsPage() {
+  return <DatadogSaving />
 }
-export default DatadogSaving
