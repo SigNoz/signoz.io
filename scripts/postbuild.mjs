7ac16ca8f55d634a1f82acb321a0a3758f197098
Yunus A M <myounis.ar@live.com>
2024-06-07 22:39:32 +0530

    chore: remove generateTags script from postbuild script

--- scripts/postbuild.mjs
+++ scripts/postbuild.mjs
@@ -1,10 +1,8 @@
 import searchIndexes from './generateIndexes.mjs'
-import tags from './generateTags.mjs'
 import rss from './rss.mjs'
 
 async function postbuild() {
-  await searchIndexes();
-  await tags();
+  await searchIndexes()
   await rss()
 }
 
