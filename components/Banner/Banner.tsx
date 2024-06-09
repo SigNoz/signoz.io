b7aba1522a10f808baee968e076f6220642ff2d2
Harsh Narayan <itsharshnarayan@gmail.com>
2024-06-09 21:28:23 +0100

    feat:Add banner to homepage

---
+++ components/Banner/Banner.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useState } from 'react';
+import { X } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+const Banner = () => {
+    const [visible, setVisible] = useState(true);
+    const pathname = usePathname();
+    const [isHomePage, setIsHomePage] = useState(false);
+
+    useState(() => {
+        setIsHomePage(pathname === '/');
+    }, [pathname]);
+
+    if (!visible || !isHomePage) return null;
+
+    return (
+        <div className="w-full bg-indigo-500 flex items-center justify-center py-2">
+            <div className="w-full flex justify-between items-center px-4 md:justify-center">
+                <Link href="https://signoz.io/blog/kafka-monitoring-opentelemetry/" className="text-base font-medium text-white leading-5">
+                    Deep dive into observability of Messaging Queues with OpenTelemetry
+                </Link>
+                <button className="text-white md:absolute md:right-4" onClick={() => setVisible(false)}>
+                    <X size={16} />
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
