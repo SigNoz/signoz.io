7cfe0d74654ae91331488ddc1220f083cd9b76e1
grandwizard28 <vibhupandey28@gmail.com>
2024-07-04 14:44:05 +0530

    docs(ingestion): add metrics and traces as well

--- constants/docsSideNav.ts
+++ constants/docsSideNav.ts
@@ -962,7 +962,6 @@ const docsSideNav = [
     label: 'Ingestion',
     type: 'category',
     items: [
-      //     'userguide/overview',
       {
         type: 'doc',
         route: '/docs/ingestion/ingestion.mdx',
@@ -972,6 +971,16 @@ const docsSideNav = [
         type: 'doc',
         route: '/docs/ingestion/ingestion-logs.mdx',
         label: 'Ingestion Logs',
+      },
+      {
+        type: 'doc',
+        route: '/docs/ingestion/ingestion-traces.mdx',
+        label: 'Ingestion Traces',
+      },
+      {
+        type: 'doc',
+        route: '/docs/ingestion/ingestion-metrics.mdx',
+        label: 'Ingestion Metrics',
       }
     ],
   },
