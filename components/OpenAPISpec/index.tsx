e068d4baccbc3d11734ad329f9a685af57ee1fdf
YounixM <myounis.ar@live.com>
2024-07-14 17:15:30 +0530

    feat: add permanent re-route

--- components/OpenAPISpec/index.tsx
+++ components/OpenAPISpec/index.tsx
@@ -6,9 +6,5 @@ import '@stoplight/elements/styles.min.css'
 import React from 'react'
 
 export default function OpenAPISpec() {
-  return (
-    <div className="api-reference" data-theme="dark">
-      <API apiDescriptionUrl="/openAPISpec/alerts.yaml" router="hash" />{' '}
-    </div>
-  )
+  return <API apiDescriptionUrl="/openAPISpec/alerts.yaml" router="hash" layout="responsive" />
 }
