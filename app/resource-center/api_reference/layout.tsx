67170b7c437164d236cf16da72ac26df0d948289
ahmadshaheer <ashaheerki@gmail.com>
2024-07-15 09:12:18 +0430

    use custom layout for api_reference

---
+++ app/resource-center/api_reference/layout.tsx
@@ -0,0 +1,100 @@
+import 'css/tailwind.css'
+import 'css/global.css'
+import 'pliny/search/algolia.css'
+
+import { GoogleTagManager } from '@next/third-parties/google'
+import { SearchProvider, SearchConfig } from 'pliny/search'
+import SectionContainer from '@/components/SectionContainer'
+import siteMetadata from '@/data/siteMetadata'
+import { ThemeProviders } from 'app/theme-providers'
+import { Metadata } from 'next'
+import MainFooter from '@/components/mainFooter'
+import TopNav from '@/components/TopNav/TopNav'
+import { Inter } from 'next/font/google'
+import React from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  metadataBase: new URL(siteMetadata.siteUrl),
+  title: {
+    default: siteMetadata.title,
+    template: `%s | ${siteMetadata.title}`,
+  },
+  description: siteMetadata.description,
+  openGraph: {
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    url: './',
+    siteName: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+    locale: 'en_US',
+    type: 'website',
+  },
+  alternates: {
+    canonical: './',
+    types: {
+      'application/rss+xml': `${siteMetadata.siteUrl}/feed.xml`,
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  twitter: {
+    title: siteMetadata.title,
+    card: 'summary_large_image',
+    images: [siteMetadata.socialBanner],
+  },
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html
+      lang={siteMetadata.language}
+      className={`${inter.className} scroll-smooth`}
+      suppressHydrationWarning
+    >
+      <GoogleTagManager gtmId="GTM-N9B6D4H" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
+      <link rel="manifest" href="/static/favicons/site.webmanifest" />
+      <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
+      <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
+      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+
+      <body className="bg-signoz_ink-500 pl-[calc(100vw-100%)] text-white antialiased">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N9B6D4H"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
+        <ThemeProviders>
+          <SectionContainer>
+            <div className="relative flex h-screen flex-col justify-between ">
+              <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+                <TopNav />
+                <main className="mb-auto mt-[48px] overflow-y-auto" style={{minHeight: "95vh"}}>{children}</main>
+              </SearchProvider>
+              <MainFooter />
+            </div>
+          </SectionContainer>
+        </ThemeProviders>
+      </body>
+    </html>
+  )
+}
