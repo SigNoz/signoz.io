d5e55ee85ef65780f180a5d2ea12b164f30cb96e
Harsh Narayan <itsharshnarayan@gmail.com>
2024-07-15 14:39:13 +0100

    feat: add info cards

--- app/teams/TestimonialSection.tsx
+++ app/teams/TestimonialSection.tsx
@@ -1,26 +1,83 @@
 import React from 'react'
 import './styles.module.css'
+import { Snowflake, ShieldCheck, Telescope, Globe } from 'lucide-react'
 
 const TestimonialSection: React.FC = () => {
   return (
     <div className="signup-testimonial flex flex-col max-md:w-full lg:w-[30%] xl:w-[40%]">
       <div className="signup-testimonial-bg" />
       <div className="z-10 my-auto flex flex-col items-start self-stretch p-16 px-12 py-32 text-xl text-stone-300 max-md:mt-10 max-md:max-w-full">
-        <img
-          loading="lazy"
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/1ed7d4d9c492e5c7d2509409a4e7ecc93b7a259d8489c7ac37ac6871917c6f13?apiKey=f0103e73688241f896979b7df0e7cb45&"
-          className="aspect-[4] w-28 max-w-full"
-          alt="SigNoz logo"
-        />
-        <div className="testimonial-content mt-12 self-stretch text-signoz_sienna-200 max-md:mt-10 max-md:max-w-full">
-          SigNoz balances flexibility and security extremely well. They've built clean and
-          thoughtful abstractions over advanced security foundations, and the product just works. We
-          barely have to think about it.
+        <div className="flex flex-col justify-between">
+          <div>
+            <div className="flex flex-col justify-center gap-y-4">
+              <div className="w-full rounded border border-signoz_slate-500 bg-signoz_ink-400 p-4">
+                <div className='flex gap-2'>
+                  <div className='py-1'>
+                    <Snowflake size={16} color='#fff' />
+                  </div>
+                  <div className='flex flex-col'>
+
+                    <p className="mb-2 text-base font-medium text-signoz_vanilla-100">
+                      Three signals in single pane
+                    </p>
+                    <p className="m-0 text-sm font-normal leading-9 text-signoz_vanilla-400">
+                      Get logs, metrics, and traces under a single pane of glass. No need for disparate tooling with too much operational overhead.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="w-full rounded border border-signoz_slate-500 bg-signoz_ink-400 p-4">
+                <div className='flex gap-2'>
+                  <div className='py-1'>
+                    <ShieldCheck size={16} color="#fff" />
+                  </div>
+                  <div className='flex flex-col'>
+
+                    <p className="mb-2 text-base font-medium text-signoz_vanilla-100">
+                      Trusted by industry leaders
+                    </p>
+                    <p className="m-0 text-sm font-normal leading-9 text-signoz_vanilla-400">
+                      Teams at NetApp, ClearTax and other industry leaders have trusted SigNoz to run their observability stack.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full rounded border border-signoz_slate-500 bg-signoz_ink-400 p-4">
+                <div className='flex gap-2'>
+                  <div className='py-1'>
+                    <Telescope size={16} color="#fff" />
+                  </div>
+                  <div className='flex flex-col'>
+
+                    <p className="mb-2 text-base font-medium text-signoz_vanilla-100">
+                      OpenTelemetry-native
+                    </p>
+                    <p className="m-0 text-sm font-normal leading-9 text-signoz_vanilla-400">
+                      Future-proof your observability with OpenTelemetry. Second most active CNCF project after Kubernetes, OpenTelemetry is the future of cloud-native application monitoring.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full rounded border border-signoz_slate-500 bg-signoz_ink-400 p-4">
+                <div className='flex gap-2'>
+                  <div className='py-1'>
+                    <Globe size={16} color="#fff" />
+                  </div>
+                  <div className='flex flex-col'>
+
+                    <p className="mb-2 text-base font-medium text-signoz_vanilla-100">
+                      Data Residency
+                    </p>
+                    <p className="m-0 text-sm font-normal leading-9 text-signoz_vanilla-400">
+                      Worried about data privacy and regulation laws? We have data centers in EU, US and India region to help you comply with data privacy regulation.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <footer className="mt-12 text-[20px] text-signoz_sienna-200 max-md:mt-10">
-          <cite className="text-center font-medium not-italic leading-[140%]">Mike Hudak</cite>
-          <p className="mt-2 text-center text-base leading-6">CTO ⎯ incident.io</p>
-        </footer>
       </div>
     </div>
   )
