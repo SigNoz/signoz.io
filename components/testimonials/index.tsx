9f3b15c5022b7a9f088f629d94ba718f5f841073
harshbutfairx <itsharshnarayan@gmail.com>
2024-07-18 03:12:36 +0100

    shadow on last 2 cards (#494)
    * fix: shadow on last 2 cards
    
    * fix: shadow effect

--- components/testimonials/index.tsx
+++ components/testimonials/index.tsx
@@ -226,12 +226,14 @@ export const Testimonials = () => {
             </div>
           ))}
         </div>
-        <Link href="/case-study/" className="shadow-[-10px_-250px_200px_370px_rgba(11,12,14,0.9)]">
+        <div className="w-[52vw] shadow-[-4px_-40px_80px_146px_rgba(11,12,14,0.8)]">
+        <Link href="/case-study/" className='flex justify-center'>
           <button className="button-background flex h-8 items-center justify-center gap-1.5 truncate rounded-full px-4 py-2 pl-4 pr-3 text-center text-sm font-medium not-italic leading-5 text-white no-underline outline-none hover:text-white">
             Read customer stories
             <ArrowRight size={14} />
           </button>
         </Link>
+        </div>
       </div>
     </section>
   )
