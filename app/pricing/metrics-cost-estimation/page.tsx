b2576dab9edcdc71f74fc9f7402cb63043c28afb
harshbutfairx <itsharshnarayan@gmail.com>
2024-07-22 08:13:32 +0100

    fix: metadata title layout for docs (#519)

--- app/pricing/metrics-cost-estimation/page.tsx
+++ app/pricing/metrics-cost-estimation/page.tsx
@@ -3,7 +3,9 @@ import MetricsCostEstimation from './MetricsCostEstimation'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Metrics Cost Estimation',
+  title: {
+    absolute: 'SigNoz | Metrics Cost Estimation',
+  },
   openGraph: {
     title: 'SigNoz | Metrics Cost Estimation',
     description: 'Understand the pricing of metrics in SigNoz cloud.',
