b8de2949abf2fc103756d32c576d4d1ab999f91c
CheetoDa <31571545+Calm-Rock@users.noreply.github.com>
2024-08-05 18:02:38 +0530

    feat: added changelog in footer (#598)
    * feat: added changelog in footer
    
    * chore: added changelog to footer community section

--- components/mainFooter.tsx
+++ components/mainFooter.tsx
@@ -63,6 +63,9 @@ function Footer() {
                   </Link>
                   <ArrowUpRight size={16} />
                 </div>
+                <div className="mt-5 flex items-center gap-2 pr-7 hover:underline max-md:pr-5">
+                  <Link href="/changelog">Changelog</Link>
+                </div>
               </div>
             </div>
             <div className="ml-5 flex w-3/12 flex-col max-md:ml-0 max-md:w-full">
