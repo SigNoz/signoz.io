fbd75a2f1ad9e0a505912d3057c0f9ee0318d885
CheetoDa <31571545+Calm-Rock@users.noreply.github.com>
2024-08-08 12:05:08 +0530

    chore: removed banner (#604)

--- components/TopNav/TopNav.tsx
+++ components/TopNav/TopNav.tsx
@@ -66,7 +66,7 @@ export default function TopNav() {
   return (
     <div className="fixed left-0 right-0 z-30">
 
-      <Banner/>
+      {/* <Banner/> */}
 
       <header
         className={`header-bg mx-auto box-border flex h-[56px] w-full items-center border-b border-signoz_slate-500 px-4 text-signoz_vanilla-100 backdrop-blur-[20px] dark:text-signoz_vanilla-100 md:px-8 lg:px-8`}
