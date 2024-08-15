3412818168d1b3fc3d7ae58797a77fac6e9b7b4d
Yunus M <myounis.ar@live.com>
2024-08-15 23:28:37 +0530

    feat: integrate storybook and setup base for stories with examples

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-dark-mode'
+  ],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {},
+  },
+  staticDirs: ['../public'],
+}
+export default config
