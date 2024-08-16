d7add7aca82fe6950b355cdfe5b0a8c5d7d06dfc
Yunus M <myounis.ar@live.com>
2024-08-16 15:02:21 +0530

    chore: add @emotion/is-prop-valid

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 /** @type { import('@storybook/nextjs').StorybookConfig } */
 const config = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
     '@storybook/addon-links',
