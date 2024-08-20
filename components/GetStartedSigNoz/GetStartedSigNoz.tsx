88b0c7d9c001bb861752657f359cea86344169f4
Ankit Anand <83692067+ankit01-oss@users.noreply.github.com>
2024-08-20 20:14:54 +0530

    minor changes to logs feature page (#653)
    * minor changes to logs feature page
    
    * Updated live logs section
    
    * Updated parse logs section
    
    ---------
    
    Co-authored-by: ankit01-oss <>

--- components/GetStartedSigNoz/GetStartedSigNoz.tsx
+++ components/GetStartedSigNoz/GetStartedSigNoz.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const markdownContent = `SigNoz cloud is the easiest way to run SigNoz. [Sign up](https://signoz.io/teams/) for a free
 account and get 30 days of unlimited access to all features. [![Try SigNoz Cloud
 CTA](/img/blog/common/try-signoz-cloud-all-blog-cta.webp)](https://signoz.io/teams/) You can
-also install and self-host SigNoz yourself since it is open-source. With 16,000+ GitHub stars,
+also install and self-host SigNoz yourself since it is open-source. With 18,000+ GitHub stars,
 [open-source SigNoz](https://github.com/signoz/signoz) is loved by developers. Find the
 [instructions](https://signoz.io/docs/install/) to self-host SigNoz.
 `
