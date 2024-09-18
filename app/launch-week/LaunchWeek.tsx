d7aa108e1aad7e6b9b27f96cc83f52cb82b13e45
Ankit Anand <83692067+ankit01-oss@users.noreply.github.com>
2024-09-18 18:18:13 +0530

    Changes for day 3 launch (#788)
    Co-authored-by: ankit01-oss <>

--- app/launch-week/LaunchWeek.tsx
+++ app/launch-week/LaunchWeek.tsx
@@ -148,14 +148,57 @@ const MainSection: React.FC = () => {
 
 
 
+          <div className='flex flex-col sm:flex-row justify-between py-6 px-6 border-b border-t border-signoz_slate-100 border-dashed'>
+            <div className='flex flex-col justify-between gap-4 sm:pr-4'>
+              <div className='text-base uppercase text-signoz_vanilla-400'>Wednesday ⎯ Sept 18</div>
+              <div className='flex flex-col gap-2'>
+                <Link href="https://www.linkedin.com/events/signozlaunchweek2-0-day3-correl7241806894388895744/theater/" target='_blank' id="btn-linkedin-live" className='flex px-3 py-2 sm:mb-0 w-full items-center gap-2 rounded-full button-background justify-center'>
+                  <Linkedin className='w-4 h-4' />
+                  <span className='text-[#F7F7F8] text-sm font-medium whitespace-nowrap'>LinkedIn Live</span>
+                </Link>
+                <div id="btn-watch-youtube-video" className='flex px-3 py-2 mb-2 sm:mb-0 w-full items-center gap-2 rounded-full button-background cursor-pointer justify-center' onClick={onOpen}>
+                  <img src='/svgs/icons/watch-video.svg' alt='watch video icon' />
+                  <span className='text-[#F7F7F8] text-sm font-medium whitespace-nowrap'>Watch the video</span>
+                </div>
+              </div>
+            </div>
+            <Link href="/blog/introducing-correlation-of-signals/" className='flex sm:flex-row flex-col justify-between max-w-full py-4 px-5 launch-week-card-background hover:bg-[#121317] transition-colors duration-300 w-[864px] rounded-md border border-signoz_slate-500 cursor-pointer'>
+              <img src='/img/blog/2024/09/introducing-correlation-of-signals-correlation-blog-poster.webp' className=' w-auto sm:w-3/5 h-auto pr-2 sm:pr-0 mb-4 sm:mb-0' />
+              <div className='flex flex-col justify-between group'>
+                <div>
+                  <div className='text-base font-eedium mb-2'>Introducing Correlation</div>
+                  <div className='text-sm font-medium text-signoz_vanilla-400'>Bringing Infra/APM Metrics and Logs Together</div>
+                </div>
+                <div className='flex justify-end'>
+                  <div className='rounded-full button-background p-2 flex items-center justify-center w-fit h-fit transform transition-transform group-hover:translate-x-2.5 mt-2 sm:mt-0'>
+                    <ArrowRight size={14} />
+                  </div>
+                </div>
+              </div>
+            </Link>
+            <Modal size={'5xl'} backdrop="blur" isOpen={isOpen} onOpenChange={onOpenChange} className='self-center'>
+              <ModalContent className="bg-transparent">
+                {() => (
+                  <>
+                    <ModalBody className="py-10">
+                      <Youtube id="_Mpbv1imSEE" />
+                    </ModalBody>
+                  </>
+                )}
+              </ModalContent>
+            </Modal>
+          </div>
 
-          {['Wednesday ⎯ Sept 18', 'Thursday ⎯ Sept 19', 'Friday ⎯ Sept 20'].map((day, index) => (
+
+
+
+          {['Thursday ⎯ Sept 19', 'Friday ⎯ Sept 20'].map((day, index) => (
             <div key={index} className="flex flex-grow items-center py-2 px-6 justify-between border-b border-dashed border-signoz_slate-100">
               <div className="text-sm sm:text-base font-mono font-medium text-signoz_vanilla-400 uppercase w-[32%]">
                 {day}
               </div>
               <div className="w-[68%] flex justify-end sm:justify-start">
-                <CountdownTimer eventDate={`2024-09-${18 + index} 21:30:00`} />
+                <CountdownTimer eventDate={`2024-09-${19 + index} 21:30:00`} />
               </div>
             </div>
           ))}
