f97f13b71f0798d89f0e91be6af0c709fdb8aacc
ankit01-oss <>
2024-09-21 11:38:52 +0530

    Removed launch week banner and add launch week link in footer

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -16,18 +16,22 @@ const Banner = () => {
 
     if (!visible || !isHomePage) return null;
 
-    return (
-        <div className="w-full bg-indigo-500 flex items-center justify-center py-2">
-            <div className="w-full flex justify-between items-center px-4 md:justify-center">
-                <Link href="/launch-week/" className="text-base font-medium text-white leading-5">
-                    Join us for a week of exciting new features - SigNoz Launch Week 2.0 🚀
-                </Link>
-                <button className="text-white md:absolute md:right-4" onClick={() => setVisible(false)}>
-                    <X size={16} />
-                </button>
-            </div>
-        </div>
-    );
+    //Uncomment the below code to launch a banner on homepage and comment our return null
+
+    // return (
+    //     <div className="w-full bg-indigo-500 flex items-center justify-center py-2">
+    //         <div className="w-full flex justify-between items-center px-4 md:justify-center">
+    //             <Link href="/launch-week/" className="text-base font-medium text-white leading-5">
+    //                 Join us for a week of exciting new features - SigNoz Launch Week 2.0 🚀
+    //             </Link>
+    //             <button className="text-white md:absolute md:right-4" onClick={() => setVisible(false)}>
+    //                 <X size={16} />
+    //             </button>
+    //         </div>
+    //     </div>
+    // );
+
+    return null;
 };
 
 export default Banner;
