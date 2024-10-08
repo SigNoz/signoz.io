8b055d71edb88e904611c00a70ac131b08a88c92
Harsh Narayan <itsharshnarayan@gmail.com>
2024-10-08 16:40:36 +0530

    feat: add kubecon page

---
+++ app/events/kubecon-cloudnativecon-north-america-2024/page.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import MainSection from './Kubecon'
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: {
+    absolute: 'Launch Week 2.0 | Sep 16 - 20 | 9 AM PT',
+  },
+  openGraph: {
+    title: 'Launch Week 2.0 | Sep 16 - 20 | 9 AM PT',
+    description: 'Join us for a week of announcing new features at 9 AM PT daily.',
+    images: '/img/launch_week/launch-week-cover.webp'
+  },
+  description:
+    'Join us for a week of announcing new features at 9 AM PT daily.',
+  twitter: {
+    title: 'Launch Week 2.0 | Sep 16 - 20 | 9 AM PT',
+    description: 'Join us for a week of announcing new features at 9 AM PT daily.',
+    images: '/img/launch_week/launch-week-cover.webp'
+  }
+}
+
+export default function LaunchWeekPage() {
+  return <MainSection />
+} 
