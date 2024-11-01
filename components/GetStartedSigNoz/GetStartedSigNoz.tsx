65cd09889437fbe187acde5e23d641fc76275e32
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2024-11-01 15:27:39 +0530

    update get started signoz (#933)
    * update
    
    * update
    
    * merged
    
    * update
    
    * update
    
    * build
    
    * build

--- components/GetStartedSigNoz/GetStartedSigNoz.tsx
+++ components/GetStartedSigNoz/GetStartedSigNoz.tsx
@@ -2,9 +2,11 @@ import MarkdownRenderer from '@/components/ReactMarkdown'
 import React from 'react'
 
 const markdownContent = `SigNoz cloud is the easiest way to run SigNoz. [Sign up](https://signoz.io/teams/) for a free
-account and get 30 days of unlimited access to all features. [![Get Started - Free
-CTA](/img/blog/common/try-signoz-cloud-all-blog-cta.webp)](https://signoz.io/teams/) You can
-also install and self-host SigNoz yourself since it is open-source. With 18,000+ GitHub stars,
+account and get 30 days of unlimited access to all features. 
+
+[![Get Started - Free CTA](/img/launch_week/try-signoz-cloud-blog-cta.png)](https://signoz.io/teams/) 
+
+You can also install and self-host SigNoz yourself since it is open-source. With 18,000+ GitHub stars,
 [open-source SigNoz](https://github.com/signoz/signoz) is loved by developers. Find the
 [instructions](https://signoz.io/docs/install/) to self-host SigNoz.
 `
