ad44be6e0e2fabc6829c350be55e9288fe03835e
Pranay Prateek <pranay@signoz.io>
2025-01-13 13:39:50 +0530

    Update community slack link (#1086)

--- next.config.js
+++ next.config.js
@@ -136,7 +136,7 @@ module.exports = () => {
         {
           source: '/slack/',
           destination:
-            'https://join.slack.com/t/signoz-community/shared_invite/zt-2tgblijg2-eZCUAJnmp~G0ekNIv3se1A',
+            'https://join.slack.com/t/signoz-community/shared_invite/zt-2xvif5l9t-FjtES9tiFmSspV_bWL5zAA',
           basePath: false,
           permanent: true,
         },
