13100470fd383362c2709b24c4da41cc08f75c87
Vikrant Gupta <vikrant.thomso@gmail.com>
2025-01-21 17:45:26 +0530

    feat(launch-week): launch week 3.0 page  (#1100)
    * feat(launch-week): embed luma script and content changes for launch week 3
    
    * feat(launch-week): embed luma script and content changes for launch week 3
    
    * feat(launch-week): embed luma script and content changes for launch week 3
    
    * feat(launch-week): some test
    
    * feat(launch-week): some test
    
    * feat(launch-week): should be fixed now

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,45 +1,52 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 interface TimerProps {
-    eventDate: string;
+  eventDate: string
 }
 
 const CountdownTimer: React.FC<TimerProps> = ({ eventDate }) => {
-    const [timeLeft, setTimeLeft] = useState<number>(0);
+  const [timeLeft, setTimeLeft] = useState<string>('')
 
-    useEffect(() => {
+  useEffect(() => {
+    const targetDate = new Date(eventDate).getTime()
 
-        const targetDate = new Date(eventDate).getTime();
+    const intervalId = setInterval(() => {
+      const now = new Date().getTime()
+      const difference = Math.floor((targetDate - now) / 1000)
 
-        const intervalId = setInterval(() => {
-            const now = new Date().getTime();
-            const difference = Math.floor((targetDate - now) / 1000);
+      if (difference <= 0) {
+        clearInterval(intervalId)
+        setTimeLeft('')
+      } else {
+        const days = Math.floor(difference / 86400)
+        const hours = Math.floor((difference % 86400) / 3600)
+        const minutes = Math.floor(((difference % 86400) % 3600) / 60)
+        const seconds = difference % 60
 
-            if (difference <= 0) {
-                clearInterval(intervalId);
-                setTimeLeft(0);
-            } else {
-                setTimeLeft(difference);
-            }
-        }, 1000);
+        const formattedTime = `${days}d ${hours}h ${minutes}m ${seconds}s`
+        setTimeLeft(formattedTime)
+      }
+    }, 1000)
 
-        return () => clearInterval(intervalId);
-    }, [eventDate]);
+    return () => clearInterval(intervalId)
+  }, [eventDate])
 
-    return (
-        <div className="text-signoz_vanilla-400 w-[68%]">
-            {timeLeft > 0 ? (
-                <>
-                    <span className="text-sm sm:text-base text-signoz_slate-50 font-mono font-bold">COMING UP IN :</span> 
-                    <span className="font-mono ml-2 ">{timeLeft}</span>
-                </>
-            ) : (
-                'Event Started!'
-            )}
-        </div>
-    );
-};
+  return (
+    <div className="w-[68%] text-signoz_vanilla-400">
+      {timeLeft != '' ? (
+        <>
+          <span className="font-mono text-sm font-bold text-signoz_slate-50 sm:text-base">
+            LIVE IN :
+          </span>
+          <span className="ml-2 font-mono ">{timeLeft}</span>
+        </>
+      ) : (
+        'Event Started!'
+      )}
+    </div>
+  )
+}
 
-export default CountdownTimer;
+export default CountdownTimer
