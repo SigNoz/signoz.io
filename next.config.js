b53cfe38bc101b0c87fb12bc6ed88e00fdb0bf26
CheetoDa <31571545+Calm-Rock@users.noreply.github.com>
2025-01-27 22:10:20 +0530

    fixes broken link (#1128)
    * fixes broken link
    
    * setup redirect
    
    * fix
    
    * fix
    
    * fix

--- next.config.js
+++ next.config.js
@@ -468,6 +468,11 @@ module.exports = () => {
           destination: '/docs/install/self-host',
           permanent: true,
         },
+        {
+          source: '/docs/self-host/install',
+          destination: '/docs/install/self-host',
+          permanent: true,
+        },
       ]
     },
     webpack: (config, options) => {
