b497b61c15683713efb1caeef1b73853171d8d2c
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2025-02-04 15:26:24 +0530

    changed meta image

--- app/why-opentelemetry/metadata.ts
+++ app/why-opentelemetry/metadata.ts
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
     description: 'Learn why OpenTelemetry is the future of observability. Break free from vendor lock-in, get consistent telemetry data, and future-proof your observability stack with OpenTelemetry.',
     images: [
       {
-        url: '/img/why-opentelemetry/signoz-apm.webp',
+        url: '/img/why-opentelemetry/cover.webp',
         width: 1200,
         height: 630,
         alt: 'OpenTelemetry with SigNoz',
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'Why OpenTelemetry? Top Reasons to Use OpenTelemetry | SigNoz',
     description: 'Learn why OpenTelemetry is the future of observability. Break free from vendor lock-in, get consistent telemetry data, and future-proof your observability stack with OpenTelemetry.',
-    images: ['/img/why-opentelemetry/signoz-apm.webp'],
+    images: ['/img/why-opentelemetry/cover.webp'],
   },
   alternates: {
     canonical: 'https://signoz.io/why-opentelemetry',
