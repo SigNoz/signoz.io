8e4e09e7052b40b4c4cefb7a844c34de43fa7210
vikrantgupta25 <vikrant.thomso@gmail.com>
2025-03-15 19:03:28 +0530

    chore: add banner for major upgrade across website

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -7,29 +7,29 @@ import { usePathname } from 'next/navigation';
 
 const Banner = () => {
     const [visible, setVisible] = useState(true);
-    const pathname = usePathname();
-    const [isHomePage, setIsHomePage] = useState(false);
+    // const pathname = usePathname();
+    // const [isHomePage, setIsHomePage] = useState(false);
 
-    useEffect(() => {
-        setIsHomePage(pathname === '/');
-    }, [pathname]);
+    // useEffect(() => {
+    //     setIsHomePage(pathname === '/');
+    // }, [pathname]);
 
-    if (!visible || !isHomePage) return null;
+    if (!visible) return null;
 
     //Uncomment the below code to launch a banner on homepage and comment our return null
 
-    // return (
-    //     <div className="w-full bg-indigo-500 flex items-center justify-center py-2">
-    //         <div className="w-full flex justify-between items-center px-4 md:justify-center">
-    //             <Link href="https://lu.ma/1z2jr0yy" className="text-base font-medium text-white leading-5">
-    //              Join us for SigNoz Community call on 19th February 🚀
-    //             </Link>
-    //             <button className="text-white md:absolute md:right-4" onClick={() => setVisible(false)}>
-    //                 <X size={16} />
-    //             </button>
-    //         </div>
-    //     </div>
-    // );
+    return (
+        <div className="w-full bg-indigo-500 flex items-center justify-center py-2">
+            <div className="w-full flex justify-between items-center px-4 md:justify-center">
+                <Link href="https://github.com/SigNoz/signoz/releases/tag/v0.76.0" className="text-base font-medium text-white leading-5">
+                    🚨 Major upgrade v0.76.0! Introducing a single binary for easier open source installation and maintenance
+                </Link>
+                <button className="text-white md:absolute md:right-4" onClick={() => setVisible(false)}>
+                    <X size={16} />
+                </button>
+            </div>
+        </div>
+    );
 
     return null;
 };
