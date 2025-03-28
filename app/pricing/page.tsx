c7a7f5c09666b62de8c2f70ace20733c9b369aa1
vikrantgupta25 <vikrant.thomso@gmail.com>
2025-03-28 20:19:12 +0530

    chore(chatwoot): integrate chatwoot in website

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Pricing from './Pricing'
 import { Metadata } from 'next'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: {
@@ -8,12 +9,36 @@ export const metadata: Metadata = {
   },
   openGraph: {
     title: 'SigNoz | Pricing',
-    description: 'Explore SigNoz plans and pricing. Transparent & predictable with only usage-based pricing. No user-based pricing, no pricing based on containers, hosts, or nodes. No special pricing for custom metrics.',
+    description:
+      'Explore SigNoz plans and pricing. Transparent & predictable with only usage-based pricing. No user-based pricing, no pricing based on containers, hosts, or nodes. No special pricing for custom metrics.',
   },
   description:
     'Explore SigNoz plans and pricing. Transparent & predictable with only usage-based pricing. No user-based pricing, no pricing based on containers, hosts, or nodes. No special pricing for custom metrics.',
 }
 
 export default function PricingPage() {
-  return <Pricing />
+  return (
+    <>
+      <Script
+        id="chatwoot-init"
+        dangerouslySetInnerHTML={{
+          __html: `(function(d,t) {
+    var BASE_URL="https://app.chatwoot.com";
+    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=BASE_URL+"/packs/js/sdk.js";
+    g.defer = true;
+    g.async = true;
+    s.parentNode.insertBefore(g,s);
+    g.onload=function(){
+      window.chatwootSDK.run({
+        websiteToken: ${process.env.NEXT_PUBLIC_CHATWOOT_TOKEN},
+        baseUrl: BASE_URL
+      })
+    }
+  })(document,"script")`,
+        }}
+      />
+      <Pricing />
+    </>
+  )
 }
