569e7137e52f43aa40788be5ed93c40d663ce4fb
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2025-03-31 19:26:07 +0530

    remove posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,6 @@ import MainFooter from '@/components/mainFooter'
 import TopNav from '@/components/TopNav/TopNav'
 import { Inter } from 'next/font/google'
 import React, { Suspense } from 'react'
-import { PostHogProvider } from './providers'
 import MixpanelClientInitializer from './mixpanel-initializer'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -81,23 +80,21 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           ></iframe>
         </noscript>
 
-        <PostHogProvider>
-          <MixpanelClientInitializer>
-            <ThemeProviders>
-              <Suspense>
-                <SectionContainer>
-                  <div className="relative flex h-screen flex-col justify-between ">
-                    <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
-                      <TopNav />
-                      <main className="mb-auto mt-[48px]">{children}</main>
-                    </SearchProvider>
-                    <MainFooter />
-                  </div>
-                </SectionContainer>
-              </Suspense>
-            </ThemeProviders>
-          </MixpanelClientInitializer>
-        </PostHogProvider>
+        <MixpanelClientInitializer>
+          <ThemeProviders>
+            <Suspense>
+              <SectionContainer>
+                <div className="relative flex h-screen flex-col justify-between ">
+                  <SearchProvider searchConfig={siteMetadata.search as SearchConfig}>
+                    <TopNav />
+                    <main className="mb-auto mt-[48px]">{children}</main>
+                  </SearchProvider>
+                  <MainFooter />
+                </div>
+              </SectionContainer>
+            </Suspense>
+          </ThemeProviders>
+        </MixpanelClientInitializer>
 
       </body>
     </html>
