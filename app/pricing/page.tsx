8418bba2a7dacb708ebed90d217f668f4cceb77d
Vikrant Gupta <vikrant@signoz.io>
2025-03-31 10:25:52 +0530

    Revert "chore(chatwoot): integrate chatwoot in website (#1293)"
    This reverts commit 8c0f1ddd657aee954e24586ac6ccbd95420b1c86.

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Pricing from './Pricing'
 import { Metadata } from 'next'
-import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: {
@@ -9,36 +8,12 @@ export const metadata: Metadata = {
   },
   openGraph: {
     title: 'SigNoz | Pricing',
-    description:
-      'Explore SigNoz plans and pricing. Transparent & predictable with only usage-based pricing. No user-based pricing, no pricing based on containers, hosts, or nodes. No special pricing for custom metrics.',
+    description: 'Explore SigNoz plans and pricing. Transparent & predictable with only usage-based pricing. No user-based pricing, no pricing based on containers, hosts, or nodes. No special pricing for custom metrics.',
   },
   description:
     'Explore SigNoz plans and pricing. Transparent & predictable with only usage-based pricing. No user-based pricing, no pricing based on containers, hosts, or nodes. No special pricing for custom metrics.',
 }
 
 export default function PricingPage() {
-  return (
-    <>
-      <Script
-        id="chatwoot-init"
-        dangerouslySetInnerHTML={{
-          __html: `(function(d,t) {
-    var BASE_URL="https://app.chatwoot.com";
-    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-    g.src=BASE_URL+"/packs/js/sdk.js";
-    g.defer = true;
-    g.async = true;
-    s.parentNode.insertBefore(g,s);
-    g.onload=function(){
-      window.chatwootSDK.run({
-        websiteToken: '${process.env.NEXT_PUBLIC_CHATWOOT_TOKEN}',
-        baseUrl: BASE_URL
-      })
-    }
-  })(document,"script")`,
-        }}
-      />
-      <Pricing />
-    </>
-  )
+  return <Pricing />
 }
