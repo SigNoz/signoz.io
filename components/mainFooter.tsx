90ec0581d0f84a01c10b8c7b72a6b5d440323fc9
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2025-04-14 13:34:31 +0530

    Teams Page Revamp (#1328)
    * add background image
    
    * new jsx
    
    * remove company logos
    
    * adjust layout
    
    * add margin
    
    * handle mobile menu
    
    * change step 3 copy
    
    * remove empty link
    
    * remove unnecessary comment
    
    * modify border
    
    * remove hover color
    
    * use lighter shade
    
    * radial blur
    
    * remove comments
    
    * stacked the data regions on mobile phone

--- components/mainFooter.tsx
+++ components/mainFooter.tsx
@@ -15,9 +15,10 @@ function Footer() {
   const searchParams = useSearchParams()
   const source = searchParams.get(QUERY_PARAMS.SOURCE)
   const isLoginRoute = pathname === '/login/'
+  const isSignupRoute = pathname === '/teams/'
   const isOnboardingRoute = source === ONBOARDING_SOURCE
 
-  if (isLoginRoute || isOnboardingRoute) {
+  if (isLoginRoute || isOnboardingRoute || isSignupRoute) {
     return null
   }
 
