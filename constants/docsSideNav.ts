a7a58eecd4ca655e1b4d557e2fa73e576b040bf3
CheetoDa <31571545+Calm-Rock@users.noreply.github.com>
2025-04-22 11:32:35 +0530

    feat: added send logs page (#1359)
    * feat: added send logs page
    
    * fix: minor fix
    
    * fix: minor fix

--- constants/docsSideNav.ts
+++ constants/docsSideNav.ts
@@ -794,6 +794,7 @@ const docsSideNav = [
         type: 'category',
         isExpanded: false,
         label: 'Send Logs to SigNoz',
+        route: '/docs/logs-management/send-logs-to-signoz',
         items: [
           {
             type: 'doc',
