cf41f26e0122116e0c89707295faab6db1a4154e
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2025-04-22 14:40:14 +0530

    make header server rendered

--- components/index-header/index.tsx
+++ components/index-header/index.tsx
@@ -1,17 +1,11 @@
-'use client'
-
 import Hero from '../../components/ui/Hero'
-import VimeoPlayer from '../../components/VimeoPlayer/VimeoPlayer'
 import { ArrowRight, BookOpen, Handshake } from 'lucide-react'
 import Button from '@/components/Button/Button'
-import { Modal, ModalContent, ModalBody, useDisclosure } from '@nextui-org/react'
 import TrackingLink from '@/components/TrackingLink'
-import TrackingButton from '@/components/TrackingButton'
 import Link from 'next/link'
+import { VideoModalPlayer } from './VideoModalPlayer'
 
 export const Header = () => {
-  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure()
-
   return (
     <header className="relative !mx-auto mt-16 !w-[100vw] md:!w-[80vw]">
       <div className="absolute bottom-0 left-[12px] right-[12px] top-0 z-[-1] border !border-b-0 !border-t-0 border-dashed border-signoz_slate-400 md:left-[24px] md:right-[24px]" />
@@ -68,51 +62,10 @@ export const Header = () => {
       </div>
       <div className="section-container !mx-auto !mt-0 !w-[90vw] border !border-b-0 !border-t-0 border-none border-signoz_slate-400 md:!w-[80vw] md:border-dashed">
         <div className="w-100 mx-[-28px]">
-          <div className="product-explainer-video hero-figure rounded-lg p-3">
-            <div className="embed-container">
-              <div className="absolute">
-                <img
-                  src="/img/landing/landing_thumbnail.webp"
-                  alt="Custom Thumbnail"
-                  className="w-full rounded-lg"
-                />
-
-                <div className="play-container absolute inset-0 m-auto flex h-16 w-16 cursor-pointer items-center justify-center rounded-full focus-visible:outline-none">
-                  <TrackingButton
-                    clickType="Video Click"
-                    clickName="Video Play Button"
-                    clickText="Play Video"
-                    clickLocation="Hero Section"
-                    onClick={onOpen}
-                  >
-                    <img
-                      src="/svgs/icons/play-icon.svg"
-                      alt="signoz-video-play-btn"
-                      className="h-6 w-6 md:h-20 md:w-20"
-                    />
-                  </TrackingButton>
-                </div>
-              </div>
-
-              <Modal
-                size={'5xl'}
-                backdrop="blur"
-                isOpen={isOpen}
-                onOpenChange={onOpenChange}
-                className="self-center"
-              >
-                <ModalContent className="bg-transparent">
-                  {() => (
-                    <>
-                      <ModalBody className="py-6">
-                        <VimeoPlayer videoId="944340217" />
-                      </ModalBody>
-                    </>
-                  )}
-                </ModalContent>
-              </Modal>
-            </div>
-          </div>
+          <VideoModalPlayer
+            thumbnailSrc="/img/landing/landing_thumbnail.webp"
+            videoId="944340217"
+          />
         </div>
       </div>
     </header>
