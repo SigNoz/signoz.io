793e294893cc01ae21bdd5d1ba2c3f6693a0e841
Piyush Singariya <piyushsingariya@gmail.com>
2025-04-24 15:31:51 +0530

    docs: adding docs related to configuring Azure MySQL Flexible server

--- constants/docsSideNav.ts
+++ constants/docsSideNav.ts
@@ -1892,6 +1892,11 @@ const docsSideNav = [
         label: 'SQL Database Metrics',
       },
       {
+        type: 'doc',
+        route: '/docs/azure-monitoring/mysql-flexible-server',
+        label: 'MySQL Flexible Server',
+      },
+      {
         type: 'category',
         isExpanded: false,
         label: 'Azure Blob Storage',
