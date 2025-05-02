98024f7e4df5dc3828c15f75c32396cabfadc6a9
Apurv11 <apurvon@gmail.com>
2025-05-02 16:34:46 +0530

    feat: add Jenkins metrics and traces to Signoz

--- constants/docsSideNav.ts
+++ constants/docsSideNav.ts
@@ -1588,6 +1588,23 @@ const docsSideNav = [
     ],
   },
   {
+    label: 'CI/CD',
+    type: 'category',
+    isExpanded: false,
+    items: [
+      {
+        type: 'doc',
+        route: '/docs/cicd/jenkins/agent-node-monitoring',
+        label: 'Agent Node Monitoring',
+      },
+      {
+        type: 'doc',
+        route: '/docs/cicd/jenkins/jenkins-tracing',
+        label: 'Jenkins Tracing',
+      },
+    ],
+  },
+  {
     label: 'Messaging Queues',
     type: 'category',
     isExpanded: false,
