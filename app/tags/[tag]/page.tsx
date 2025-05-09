12da7cea3d4edd1b8aa85ff57a0dfade84bac2cd
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2025-05-09 22:08:39 +0530

    Show No Post Found on Empty Tag Pages (#1430)
    * show no post found message on tags with no posts
    
    * return 404 page

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -6,6 +6,9 @@ import { allBlogs } from 'contentlayer/generated'
 import tagData from 'app/tag-data.json'
 import { genPageMetadata } from 'app/seo'
 import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
+
+export const dynamicParams = false
 
 export async function generateMetadata({ params }: { params: { tag: string } }): Promise<Metadata> {
   const tag = decodeURI(params.tag)
@@ -24,18 +27,26 @@ export async function generateMetadata({ params }: { params: { tag: string } }):
 export const generateStaticParams = async () => {
   const tagCounts = tagData as Record<string, number>
   const tagKeys = Object.keys(tagCounts)
-  const paths = tagKeys.map((tag) => ({
-    tag: encodeURI(tag),
-  }))
+  // Only generate pages for tags that have at least one post
+  const paths = tagKeys
+    .filter((tag) => tagCounts[tag] > 0)
+    .map((tag) => ({
+      tag: encodeURI(tag),
+    }))
   return paths
 }
 
 export default function TagPage({ params }: { params: { tag: string } }) {
   const tag = decodeURI(params.tag)
-  // Capitalize first letter and convert space to dash
-  const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
   const filteredPosts = allCoreContent(
     sortPosts(allBlogs.filter((post) => post.tags && post.tags.map((t) => slug(t)).includes(tag)))
   )
+
+  // Return 404 for empty tag pages
+  if (filteredPosts.length === 0) {
+    notFound() // Next.js function to return 404
+  }
+
+  const title = tag[0].toUpperCase() + tag.split(' ').join('-').slice(1)
   return <ListLayout posts={filteredPosts} title={title} />
 }
