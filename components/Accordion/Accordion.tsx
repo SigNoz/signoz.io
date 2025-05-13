dcf3f2f3e0e068541ba739787add02a2268e7f66
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2025-05-13 11:46:06 +0530

    close the accordian menu on item click (#1445)

--- components/Accordion/Accordion.tsx
+++ components/Accordion/Accordion.tsx
@@ -1,37 +1,38 @@
-import React, { useState } from 'react';
-import { ChevronDown, ChevronUp } from 'lucide-react';
-import TrackingLink from '@/components/TrackingLink';
+import React, { useState } from 'react'
+import { ChevronDown, ChevronUp } from 'lucide-react'
+import TrackingLink from '@/components/TrackingLink'
 
-const Accordion = ({ topic, subtopics }) => {
-  const [accordionIsOpen, setAccordionIsOpen] = useState(false);
+const Accordion = ({ topic, subtopics, onLinkClick }) => {
+  const [accordionIsOpen, setAccordionIsOpen] = useState(false)
 
   return (
-    <div className='-mx-3 block rounded-lg px-3 text-base font-semibold leading-7 hover:bg-[#18181B]'>
+    <div className="-mx-3 block rounded-lg px-3 text-base font-semibold leading-7 hover:bg-[#18181B]">
       <button
         onClick={() => setAccordionIsOpen(!accordionIsOpen)}
-        className='flex gap-1 items-center w-full'
+        className="flex w-full items-center gap-1"
       >
         <span>{topic}</span>
         {accordionIsOpen ? <ChevronUp size={12} /> : <ChevronDown size={12} />}
       </button>
       <div
-        className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${accordionIsOpen ? 'grid-rows-[1fr] opacity-100 py-4'
-          : 'grid-rows-[0fr] opacity-0'
-          }`}
+        className={`grid overflow-hidden text-sm text-slate-600 transition-all duration-300 ease-in-out ${
+          accordionIsOpen ? 'grid-rows-[1fr] py-4 opacity-100' : 'grid-rows-[0fr] opacity-0'
+        }`}
       >
-        <div className='overflow-hidden flex flex-col gap-4'>
+        <div className="flex flex-col gap-4 overflow-hidden">
           {subtopics.map((subtopic, index) => (
-            <TrackingLink 
-              key={index} 
-              href={subtopic.url} 
-              className='text-signoz_vanilla-300 flex items-center gap-2'
+            <TrackingLink
+              key={index}
+              href={subtopic.url}
+              className="flex items-center gap-2 text-signoz_vanilla-300"
               clickType="Nav Click"
               clickName={`${subtopic.name} Link`}
               clickText={subtopic.name}
               clickLocation={`${topic} Dropdown`}
+              onClick={() => onLinkClick && onLinkClick()}
             >
               {subtopic.icon && (
-                <img src={subtopic.icon} alt={`${subtopic.name} icon`} className="w-5 h-5" />
+                <img src={subtopic.icon} alt={`${subtopic.name} icon`} className="h-5 w-5" />
               )}
               <div className="flex flex-col">
                 <span className="font-medium">{subtopic.name}</span>
@@ -42,8 +43,7 @@ const Accordion = ({ topic, subtopics }) => {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Accordion;
-
+export default Accordion
