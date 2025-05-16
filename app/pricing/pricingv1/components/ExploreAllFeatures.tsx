d3f31cbbeb23ab833df46b057ab3bd3e0d8c8db5
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2025-05-16 16:42:30 +0530

    update feature enterprise link

--- app/pricing/pricingv1/components/ExploreAllFeatures.tsx
+++ app/pricing/pricingv1/components/ExploreAllFeatures.tsx
@@ -75,7 +75,7 @@ const ALL_FEATURES_DATA = {
       desc: 'Cloud / Self-Hosted',
       action: (
         <TrackingLink
-          href={'/enterprise-cloud/'}
+          href={'/contact-us/'}
           className="button-background flex h-8 w-full items-center justify-center gap-1.5 rounded-full px-4 py-2 text-center text-[9px] font-medium text-white sm:text-sm"
           clickType="Secondary CTA"
           clickName="Enterprise Contact Button"
