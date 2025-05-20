caee68bcb26209a2dbaaedc73bb117ad425ab417
Cruxaki <44545886+Cruxaki@users.noreply.github.com>
2025-05-20 21:24:31 +0530

    Added banner on homepage about pricing change

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -18,20 +18,20 @@ const Banner = () => {
 
   //Uncomment the below code to launch a banner on homepage and comment our return null
 
-  // return (
-  //     <div className="w-full bg-indigo-500 flex items-center justify-center py-2">
-  //         <div className="w-full flex justify-between items-center px-4 md:justify-center">
-  //             <Link href="https://lu.ma/1z2jr0yy" className="text-base font-medium text-white leading-5">
-  //              Join us for SigNoz Community call on 16th April 🚀
-  //             </Link>
-  //             <button className="text-white md:absolute md:right-4" onClick={() => setVisible(false)}>
-  //                 <X size={16} />
-  //             </button>
-  //         </div>
-  //     </div>
-  // );
+  return (
+      <div className="w-full bg-indigo-500 flex items-center justify-center py-2">
+          <div className="w-full flex justify-between items-center px-4 md:justify-center">
+              <Link href="https://signoz.io/blog/cloud-teams-plan-now-at-49usd/" className="text-base font-medium text-white leading-5">
+               We slashed our SigNoz Cloud monthly base fee by 75% - learn more. 🥳         
+               </Link>
+              <button className="text-white md:absolute md:right-4" onClick={() => setVisible(false)}>
+                  <X size={16} />
+              </button>
+          </div>
+      </div>
+  );
 
-  return null
+  // return null
 }
 
 export default Banner
