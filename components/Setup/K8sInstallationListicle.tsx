f6f800e26041ab5984a8b416990cb39041dc294d
Nageshbansal <76246968+Nageshbansal@users.noreply.github.com>
2025-06-13 19:29:06 +0530

    Refactors K8s Installation Page (#1558)

---
+++ components/Setup/K8sInstallationListicle.tsx
@@ -0,0 +1,81 @@
+'use client'
+
+import React from 'react'
+import {
+  SiAmazonwebservices,
+  SiGooglecloud,
+  SiDigitalocean,
+  SiHelm,
+  SiKubernetes,
+} from 'react-icons/si'
+import IconCardGrid from '../Card/IconCardGrid'
+
+/**
+ * Listicle for SigNoz Kubernetes installation options,
+ * based on supported docs categories.
+ */
+export default function K8sInstallationListicle() {
+  // Icons for platforms
+  const k8sIcon = <SiKubernetes className="h-7 w-7 text-blue-600" />
+  const awsIcon = <SiAmazonwebservices className="h-7 w-7 text-amber-500" />
+  const gcpIcon = <SiGooglecloud className="h-7 w-7 text-blue-400" />
+  const doIcon = <SiDigitalocean className="h-7 w-7 text-sky-400" />
+  const helmIcon = <SiHelm className="h-7 w-7 text-indigo-500" />
+
+  // Kubernetes installation cards
+  const cards = [
+    {
+      name: 'AWS',
+      href: '/docs/install/kubernetes/aws',
+      icon: awsIcon,
+      clickName: 'Deploy to AWS',
+    },
+    {
+      name: 'GCP',
+      href: '/docs/install/kubernetes/gcp',
+      icon: gcpIcon,
+      clickName: 'Deploy to GCP',
+    },
+    {
+      name: 'AKS',
+      href: '/docs/install/kubernetes/aks',
+      icon: <img src="/img/icons/azure-icon.svg" width={20} height={20} alt="Azure" />, // Azure icon
+      clickName: 'Deploy to AKS',
+    },
+    {
+      name: 'DigitalOcean',
+      href: '/docs/install/digital-ocean',
+      icon: doIcon,
+      clickName: 'Deploy to DigitalOcean',
+    },
+    {
+      name: 'Other Platform',
+      href: '/docs/install/kubernetes/others',
+      icon: helmIcon,
+      clickName: 'Deploy to Other Platform',
+    },
+    {
+      name: 'Local',
+      href: '/docs/install/kubernetes/local',
+      icon: k8sIcon,
+      clickName: 'Deploy Locally',
+    },
+    {
+      name: 'ArgoCD',
+      href: '/docs/install/argocd',
+      icon: <SiHelm className="h-7 w-7 text-gray-500" />, // Using Helm icon for ArgoCD
+      clickName: 'Deploy with ArgoCD',
+    },
+  ]
+
+  return (
+    <div className="mb-10">
+      <h2 className="mb-4 text-2xl font-semibold">Install on Kubernetes</h2>
+      <IconCardGrid
+        sectionName="Kubernetes"
+        gridCols="grid-cols-2 sm:grid-cols-3 md:grid-cols-4"
+        cards={cards}
+      />
+    </div>
+  )
+}
