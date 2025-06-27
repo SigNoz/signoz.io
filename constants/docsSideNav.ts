7ccdf5dc8db232d2aebaf4767e742deea8d86860
srikanthccv <srikanth.chekuri92@gmail.com>
2025-06-27 13:49:33 +0530

    Add docs for new QB

--- constants/docsSideNav.ts
+++ constants/docsSideNav.ts
@@ -1350,6 +1350,11 @@ const docsSideNav = [
         label: 'Query Builder',
       },
       {
+        type: 'doc',
+        route: '/docs/userguide/search-syntax',
+        label: 'Search Syntax',
+      },
+      {
         type: 'category',
         isExpanded: false,
         // route: '',
