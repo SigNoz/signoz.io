d559f4cee8ec2cfa6d98653a7574b3f52d0a280d
elizabeth <elizabethmarymathew24@gmail.com>
2025-07-31 12:24:03 +0530

    update:modifies Figure component and adds sources

--- components/Figure/Figure.tsx
+++ components/Figure/Figure.tsx
@@ -9,15 +9,16 @@ interface FigureProps {
   src: string
   alt: string
   caption: string
+  link?: string
 }
 
-export default function Figure({ src, alt, caption }: FigureProps) {
+export default function Figure({ src, alt, caption, link }: FigureProps) {
   return (
     <Zoom>
       <figure>
         <img src={src} alt={alt} />
         <figcaption>
-          <i>{caption}</i>
+          <i>{caption} {link && <a href={link} target="_blank" rel="noopener noreferrer"> Source: {link}</a>}</i>
         </figcaption>
       </figure>
     </Zoom>
