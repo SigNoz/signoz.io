9a96249ef2a46a627857651ee89bc55822b9fc9a
Tris <manika@signoz.io>
2025-08-01 16:38:33 +0530

    nit: use tw classes

--- components/GlobalLoader/GlobalLoader.tsx
+++ components/GlobalLoader/GlobalLoader.tsx
@@ -18,13 +18,13 @@ export default function GlobalLoading() {
   }, [])
 
   return (
-    <div className="loading-overlay">
-      <div className="loading-container">
-        <div className="box box-1"></div>
-        <div className="box box-2"></div>
-        <div className="box box-3"></div>
-        <div className="box box-4"></div>
+    <div className="z-10 h-screen flex items-center justify-center backdrop-blur-md bg-[rgba(60,65,82,0.1)] pointer-events-all cursor-not-allowed">
+      <div className="flex gap-2">
+        <div className="w-4 h-4 rounded-sm animate-[bounce_1s_0ms_infinite] bg-[#FF8C42]"></div>
+        <div className="w-4 h-4 rounded-sm animate-[bounce_1s_100ms_infinite] bg-[#FF7B5B]"></div>
+        <div className="w-4 h-4 rounded-sm animate-[bounce_1s_200ms_infinite] bg-[#FF6B74]"></div>
+        <div className="w-4 h-4 rounded-sm animate-[bounce_1s_300ms_infinite] bg-[#FF5A8D]"></div>
       </div>
     </div>
   )
-} 
+}
