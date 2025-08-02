f065f5b763346d511cd35893b1e50ccecf5f0e1e
srikanthccv <srikanth.chekuri92@gmail.com>
2025-08-02 16:23:57 +0530

    Keep old docs

--- constants/docsSideNav.ts
+++ constants/docsSideNav.ts
@@ -1529,6 +1529,11 @@ const docsSideNav = [
       },
       {
         type: 'doc',
+        route: '/docs/userguide/query-builder-v5',
+        label: 'Query Builder (new)',
+      },
+      {
+        type: 'doc',
         route: '/docs/userguide/search-syntax',
         label: 'Search Syntax',
       },
