8e35b28a5a1d55c23726e1adb1983a98b77611e3
amlannandy <amlannandy5@gmail.com>
2025-08-06 21:48:57 +0700

    chore: update nav

--- constants/docsSideNav.ts
+++ constants/docsSideNav.ts
@@ -1755,6 +1755,11 @@ const docsSideNav = [
         route: '/docs/alerts-management/terraform-provider-signoz',
         label: 'Terraform Provider',
       },
+      {
+        type: 'doc',
+        route: '/docs/alerts-management/apdex-alerts',
+        label: 'Apdex Alerts',
+      },
     ],
   },
   {
