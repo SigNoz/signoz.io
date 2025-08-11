960e2100ec4961be6d6c9b66d5fbde819f1c4535
Tris <manika@signoz.io>
2025-08-11 19:14:40 +0530

    test

--- app/robots.ts
+++ app/robots.ts
@@ -2,6 +2,20 @@ import { MetadataRoute } from 'next'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function robots(): MetadataRoute.Robots {
+  console.log('VERCEL_ENV', process.env.VERCEL_ENV)
+  const isProduction = process.env.VERCEL_ENV === 'production'
+  
+  if (!isProduction) {
+    return {
+      rules: {
+        userAgent: '*',
+        disallow: '/',
+      },
+      sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
+      host: siteMetadata.siteUrl,
+    }
+  }
+  
   return {
     rules: {
       userAgent: '*',
