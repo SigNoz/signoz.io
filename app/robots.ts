92a9f131a2fcb3157a670031fdd3280ebe7f2bde
Tris <manika@signoz.io>
2025-08-12 16:45:39 +0530

    use staging irrespective of url to disallow

--- app/robots.ts
+++ app/robots.ts
@@ -2,13 +2,10 @@ import { MetadataRoute } from 'next'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function robots(): MetadataRoute.Robots {
-  console.log("\nVERCEL_ENV", process.env.VERCEL_ENV)
-  console.log("\nVERCEL_URL", process.env.VERCEL_URL)
-  
   const isProduction = process.env.VERCEL_ENV === 'production'
   const currentUrl = isProduction 
     ? siteMetadata.siteUrl 
-    : `https://${process.env.VERCEL_URL}`
+    : `https://staging.signoz.io`
   
   if (!isProduction) {
     return {
