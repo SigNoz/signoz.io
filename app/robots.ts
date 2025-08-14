16dab7afe7625d7030abda8cbd8039e06ab4c86d
tris-signoz <manika@signoz.io>
2025-08-14 17:59:26 +0530

    disable crawling on non-production pages (#1815)
    * test
    
    * use vercel_url explicitly
    
    * test: add feed.xml to gitignore
    
    * use staging irrespective of url to disallow
    
    * remove host and sitemap fields from non-prod urls

--- app/robots.ts
+++ app/robots.ts
@@ -2,12 +2,26 @@ import { MetadataRoute } from 'next'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function robots(): MetadataRoute.Robots {
+  const isProduction = process.env.VERCEL_ENV === 'production'
+  const currentUrl = isProduction 
+    ? siteMetadata.siteUrl 
+    : `https://staging.signoz.io`
+  
+  if (!isProduction) {
+    return {
+      rules: {
+        userAgent: '*',
+        disallow: '/',
+      }
+    }
+  }
+  
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: `${siteMetadata.siteUrl}/sitemap.xml`,
-    host: siteMetadata.siteUrl,
+    sitemap: `${currentUrl}/sitemap.xml`,
+    host: currentUrl,
   }
 }
