4f39b1c9fb1157c2727ca944f469ae74350a77c7
manika-signoz <manika@signoz.io>
2025-08-21 10:58:25 +0530

    Force static rendering (#1831)
    * export dynamic force-static
    
    * add handling for newsroom in blog, add is_newsroom boolean for legacy
    
    * set up redirects
    
    * exclude changelog pages
    
    ---------
    
    Co-authored-by: Manika Malhotra <manikamalhotra@Manikas-MacBook-Pro.local>

--- app/docs/(main-docs)/[...slug]/page.tsx
+++ app/docs/(main-docs)/[...slug]/page.tsx
@@ -9,6 +9,7 @@ import { notFound } from 'next/navigation'
 import DocContent from '@/components/DocContent/DocContent'
 
 export const dynamicParams = false
+export const dynamic = 'force-static'
 
 export async function generateMetadata({
   params,
@@ -17,6 +18,10 @@ export async function generateMetadata({
 }): Promise<Metadata | undefined> {
   const slug = decodeURI(params.slug.join('/'))
   const post = allDocs.find((p) => p.slug === slug)
+
+  if (!post) {
+    notFound()
+  }
 
   return {
     title: post?.title,
@@ -37,8 +42,10 @@ export async function generateMetadata({
 }
 
 export const generateStaticParams = async () => {
-  const paths = allDocs.map((p) => ({ slug: p.slug?.split('/') }))
-
+  const paths = allDocs
+  .filter((p) => p.slug !== 'introduction')
+  .map((p) => ({ slug: p.slug?.split('/') })) // Don't want to generate static params for introduction page
+  
   return paths
 }
 
