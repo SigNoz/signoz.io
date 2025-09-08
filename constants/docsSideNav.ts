a1ba06e63f4fb7a58ff774a218654362a9b30c65
CheetoDa <Chitgupta24@gmail.com>
2025-09-08 16:27:37 +0530

    sidenav restructuring

--- constants/docsSideNav.ts
+++ constants/docsSideNav.ts
@@ -1476,14 +1476,27 @@ const docsSideNav = [
     // route: '',
     items: [
       {
-        type: 'doc',
-        route: '/docs/userguide/manage-dashboards',
-        label: 'Manage Dashboards',
-      },
-      {
-        type: 'doc',
-        route: '/docs/userguide/manage-panels',
-        label: 'Manage Panels',
+        label: 'Manage',
+        type: 'category',
+        className: 'new-doc',
+        isExpanded: false,
+        items: [
+          {
+            type: 'doc',
+            route: '/docs/userguide/manage-dashboards',
+            label: 'Manage Dashboards',
+          },
+          {
+            type: 'doc',
+            route: '/docs/userguide/manage-panels',
+            label: 'Manage Panels',
+          },
+          {
+            type: 'doc',
+            route: '/docs/userguide/manage-variables',
+            label: 'Manage Variables',
+          },
+        ],
       },
       {
         type: 'category',
@@ -1560,11 +1573,6 @@ const docsSideNav = [
             label: 'Filter in Tables',
           },
         ],
-      },
-      {
-        type: 'doc',
-        route: '/docs/userguide/manage-variables',
-        label: 'Manage Variables',
       },
       {
         type: 'doc',
