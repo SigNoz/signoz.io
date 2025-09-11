8ec9cb7208f72877448f8d2fb15d06425db03797
anushkasignoz <anushka@signoz.io>
2025-09-11 11:57:47 +0530

    Add Launch Week Day 4 blog post (#1949)

--- app/launch-week/LaunchWeek.tsx
+++ app/launch-week/LaunchWeek.tsx
@@ -226,7 +226,7 @@ const MainSection: React.FC = () => {
               </div>
             </div>
             <Link
-              href=""
+              href="/blog/trace-operators/"
               className="launch-week-card-background flex w-[864px] max-w-full cursor-pointer flex-col gap-6 rounded-md border border-signoz_slate-500 px-5 py-4 transition-colors duration-300 hover:bg-[#121317] sm:flex-row"
             >
               <img
@@ -244,18 +244,19 @@ const MainSection: React.FC = () => {
                 </div>
                 <div className="flex justify-end">
                   <div className="button-background mt-2 flex h-fit w-fit transform items-center justify-center rounded-full p-2 transition-transform group-hover:translate-x-2.5 sm:mt-0">
+                  <ArrowRight size={14} />
                   </div>
                 </div>
               </div>
             </Link>
           </div>
 
-          {/*<div className="flex flex-col justify-between border-b-2 border-t border-dashed border-signoz_slate-200/50 px-6 py-6 sm:flex-row">
+          <div className="flex flex-col justify-between border-b-2 border-t border-dashed border-signoz_slate-200/50 px-6 py-6 sm:flex-row">
             <div className="flex flex-col justify-between gap-4 sm:pr-4">
-              <div className="text-base uppercase text-signoz_vanilla-400">Thursday ⎯ May 15</div>
+              <div className="text-base uppercase text-signoz_vanilla-400">Thursday ⎯ Sep 11</div>
               <div className="flex flex-col gap-2">
                 <Link
-                  href="https://www.linkedin.com/events/ci-cdobservabilitypoweredbyopen7326304901754486784/theater/"
+                  href="https://www.linkedin.com/events/traceoperators-signozlaunchweek7369027260701429761/theater/"
                   target="_blank"
                   id="btn-linkedin-live"
                   className="button-background flex w-full items-center justify-center gap-2 rounded-full px-3 py-2 sm:mb-0"
@@ -268,7 +269,7 @@ const MainSection: React.FC = () => {
                 <div
                   id="btn-watch-youtube-video"
                   className="button-background mb-2 flex w-full cursor-pointer items-center justify-center gap-2 rounded-full px-3 py-2 sm:mb-0"
-                  onClick={() => handleOpenModal('L-fRIZlXYfk')}
+                  onClick={() => handleOpenModal('aSGBmAMqUHs')}
                 >
                   <img src="/svgs/icons/watch-video.svg" alt="watch video icon" />
                   <span className="whitespace-nowrap text-sm font-medium text-[#F7F7F8]">
@@ -278,33 +279,27 @@ const MainSection: React.FC = () => {
               </div>
             </div>
             <Link
-              href="/blog/ci-cd-observability-opentelemetry/"
+              href=""
               className="launch-week-card-background flex w-[864px] max-w-full cursor-pointer flex-col gap-6 rounded-md border border-signoz_slate-500 px-5 py-4 transition-colors duration-300 hover:bg-[#121317] sm:flex-row"
             >
               <img
-                src="/img/blog/2025/05/ci-cd-observability-cover.webp"
+                src="/img/blog/2025/09/trace-operators.webp"
                 className=" mb-4 h-auto w-auto pr-2 sm:mb-0 sm:w-3/5 sm:pr-0"
               />
               <div className="group flex flex-col justify-between">
                 <div>
                   <div className="font-eedium mb-2 text-base">
-                    CI/CD Observability Powered by OpenTelemetry
+                    Trace Operators
                   </div>
                   <div className="text-sm font-medium text-signoz_vanilla-400">
-                    Track and analyze every stage of your CI/CD pipeline to identify bottlenecks,
-                    improve reliability, and accelerate software delivery.
-                  </div>
-                </div>
-                <div className="flex justify-end">
-                  <div className="button-background mt-2 flex h-fit w-fit transform items-center justify-center rounded-full p-2 transition-transform group-hover:translate-x-2.5 sm:mt-0">
-                    <ArrowRight size={14} />
+                    Map service dependencies and validate architectural patterns without manually analyzing trace flows. Trace Operators let you query relationships between services within distributed traces using simple, intuitive syntax.
                   </div>
                 </div>
               </div>
             </Link>
           </div>
 
-          <div className="flex flex-col justify-between border-b-2 border-t border-dashed border-signoz_slate-200/50 px-6 py-6 sm:flex-row">
+          {/*<div className="flex flex-col justify-between border-b-2 border-t border-dashed border-signoz_slate-200/50 px-6 py-6 sm:flex-row">
             <div className="flex flex-col justify-between gap-4 sm:pr-4">
               <div className="text-base uppercase text-signoz_vanilla-400">FRIDAY ⎯ May 16</div>
               <div className="flex flex-col gap-2">
@@ -364,7 +359,7 @@ const MainSection: React.FC = () => {
               //'Monday ⎯ Sep 08',
               //'Tuesday ⎯ Sep 09',
               //'Wednesday ⎯ Sep 10',
-            'Thursday ⎯ Sep 11',
+            //'Thursday ⎯ Sep 11',
             'Friday ⎯ Sep 12',
           ].map((day, index) => (
             <div
@@ -375,7 +370,7 @@ const MainSection: React.FC = () => {
                 {day}
               </div>
               <div className="flex w-[68%] justify-end sm:justify-start">
-                <CountdownTimer eventDate={`2025-09-${11 + index} 21:30:00`} />
+                <CountdownTimer eventDate={`2025-09-${12 + index} 21:30:00`} />
               </div>
             </div>
           ))}
