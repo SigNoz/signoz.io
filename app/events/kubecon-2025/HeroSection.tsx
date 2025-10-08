e33835a08bb8ac66e427c3b394a12e7090bf1fcb
manika-signoz <manika@signoz.io>
2025-10-08 15:55:53 +0530

    hero section fix

--- app/events/kubecon-2025/HeroSection.tsx
+++ app/events/kubecon-2025/HeroSection.tsx
@@ -13,25 +13,23 @@ const hero = {
 const HeroSection = () => {
   return (
     <div className="relative">
-      <div className="container mx-auto px-4 md:px-6 lg:px-8">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 items-center">
-          <div className="space-y-6 h-full py-20 px-6 bg-gradient-to-r from-black to-black/50">
-            <div className="font-mono flex flex-col">
-              <span>{`// NOV 10 TO 13`}</span>
-              <span>{`// ATLANTA, GEORGIA`}</span>
-              <span>{`// BOOTH 1372`}</span>
+      <div className="h-full py-20 px-8 bg-gradient-to-r from-black via-black/70 to-transparent">
+        <div className="w-1/2 lg:w-1/3 space-y-6">
+          <div className="font-mono flex flex-col">
+            <span>{`// NOV 10 TO 13`}</span>
+            <span>{`// ATLANTA, GEORGIA`}</span>
+            <span>{`// BOOTH 1372`}</span>
+          </div>
+          <div className="space-y-4">
+            <div>
+              <h2 className="font-mono mb-0 text-signoz_vanilla-400">SigNoz@</h2>
+              <h1 className="font-bold">{hero.title}</h1>
             </div>
-            <div className="space-y-4">
-              <div>
-                <h2 className="font-mono mb-0 text-signoz_vanilla-400">SigNoz@</h2>
-                <h1 className="font-bold">{hero.title}</h1>
-              </div>
-              <p className="font-mono text-signoz_vanilla-300">{hero.subtitle}</p>
-              <p className="font-mono text-signoz_vanilla-300">{hero.description}</p>
-              <Button variant="default" rounded="full" href={hero.ctaLink}>
-                {hero.ctaText}
-              </Button>
-            </div>
+            <p className="font-mono text-signoz_vanilla-300">{hero.subtitle}</p>
+            <p className="font-mono text-signoz_vanilla-300">{hero.description}</p>
+            <Button variant="default" rounded="full" href={hero.ctaLink}>
+              {hero.ctaText}
+            </Button>
           </div>
         </div>
       </div>
