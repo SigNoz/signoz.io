6b4ab22a83c1bee41d7659789f5ed55b7a4fd78d
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2025-10-15 10:33:57 +0530

    change color

--- components/TableOfContents/TableOfContents.tsx
+++ components/TableOfContents/TableOfContents.tsx
@@ -91,8 +91,8 @@ const TableOfContents = ({
               data-level={tocItem.depth}
               href={tocItem.url}
               onClick={handleClick}
-              className={`line-clamp-2 text-[11px] transition-colors hover:text-primary-400 focus-visible:text-primary-400 focus-visible:outline-none ${
-                isActive ? 'font-medium text-primary-500' : 'text-gray-300'
+              className={`line-clamp-2 text-[11px] transition-colors hover:text-signoz_robin-400 focus-visible:text-signoz_robin-400 focus-visible:outline-none ${
+                isActive ? 'font-medium text-signoz_robin-500' : 'text-signoz_vanilla-300'
               }`}
             >
               {tocItem.value}
