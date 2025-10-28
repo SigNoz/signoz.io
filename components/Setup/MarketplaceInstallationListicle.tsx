94267b13229ec6706d98c62713ca4656c7f8f9a6
Nagesh <nageshbansal59@gmail.com>
2025-10-28 19:19:15 +0530

    chore(marketplace): adds support for Coolify Installation

--- components/Setup/MarketplaceInstallationListicle.tsx
+++ components/Setup/MarketplaceInstallationListicle.tsx
@@ -11,23 +11,39 @@ export default function SelfHostInstallationListicle() {
   const railwayIcon = <img src="/img/icons/railway-icon.webp" alt="Railway" />
   const doIcon = <SiDigitalocean className="h-7 w-7 text-sky-400" />
   const vultrIcon = <img src="/img/icons/vultr.svg" alt="Vultr" />
-
+  const coolifyIcon = <img src="/img/icons/coolify-logo.png" alt="Coolify" />
 
   // --- Cards for One-Click Install ---
   const oneClickCards = [
-    { name: 'Railway', href: 'https://railway.com/deploy/signoz', icon: railwayIcon, clickName: 'Deploy to Railway' },
-    { name: 'DigitalOcean', href: 'https://marketplace.digitalocean.com/apps/signoz', icon: doIcon, clickName: 'Deploy to DigitalOcean' },
-    { name: 'Vultr', href: 'https://www.vultr.com/marketplace/apps/signoz/', icon: vultrIcon, clickName: 'Deploy to Vultr' },
-
+    {
+      name: 'Railway',
+      href: 'https://railway.com/deploy/signoz',
+      icon: railwayIcon,
+      clickName: 'Deploy to Railway',
+    },
+    {
+      name: 'DigitalOcean',
+      href: 'https://marketplace.digitalocean.com/apps/signoz',
+      icon: doIcon,
+      clickName: 'Deploy to DigitalOcean',
+    },
+    {
+      name: 'Vultr',
+      href: 'https://www.vultr.com/marketplace/apps/signoz/',
+      icon: vultrIcon,
+      clickName: 'Deploy to Vultr',
+    },
+    {
+      name: 'Coolify',
+      href: 'https://github.com/coollabsio/coolify/blob/v4.x/templates/compose/signoz.yaml',
+      icon: coolifyIcon,
+      clickName: 'Deploy to Coolify',
+    },
   ]
 
   return (
     <div>
-      <IconCardGrid
-        sectionName="all"
-        gridCols="grid-cols-2 sm:grid-cols-3"
-        cards={oneClickCards}
-      />
+      <IconCardGrid sectionName="all" gridCols="grid-cols-2 sm:grid-cols-3" cards={oneClickCards} />
     </div>
   )
 }
