f4aca43134cb8d39d779b8b8d11696f0c7a3be30
dhruv-signoz <dhruv@signoz.io>
2025-12-04 11:52:10 +0530

    Modernize OTel Django Blog (#2281)
    * refactor: revamp intro
    
    - update title, date
    - remove header tag content
    - tighten the intro paragraphs
    - include few observability terms
    
    * update intro sections
    
    * (wip)refactor: bring article in-line with otel python blog
    
    * chore: remove outdated video walkthrough
    
    * update tag data
    
    * refactor: fix minor language and add logs export section
    
    * make minor corrections
    
    * fix: remove public prefix from OG images in other articles
    
    * refactor: improve writing style for several paragraphs
    
    * chore: fix grammar mistakes
    
    * feat: add walkthrough video and fix minor grammar
    
    * adjust tags and video caption
    
    * add missing determiner
    
    * feat: update OG image
    
    * feat: add youtube video instead of in-line video
    
    * chore: address initial feedback
    
    * chore: improve CTAs
    
    * chore: update star count in getStarted component
    
    * fix typo
    
    * chore: adjust the signoz callout in intro

--- components/GetStartedSigNoz/GetStartedSigNoz.tsx
+++ components/GetStartedSigNoz/GetStartedSigNoz.tsx
@@ -7,58 +7,59 @@ export default function GetStartedSigNoz() {
     <div className="get-started-signoz">
       <p>
         SigNoz cloud is the easiest way to run SigNoz.{' '}
-        <TrackingLink 
-          href="https://signoz.io/teams/" 
+        <TrackingLink
+          href="https://signoz.io/teams/"
           clickType="Nav Click"
           clickName="Sign Up Link"
           clickLocation="Get Started SigNoz Card"
           clickText="Sign up"
-          className="text-blue-600 hover:underline font-medium"
+          className="font-medium text-blue-600 hover:underline"
         >
           Sign up
         </TrackingLink>{' '}
         for a free account and get 30 days of unlimited access to all features.
       </p>
 
-      <TrackingLink 
-        href="/teams/" 
+      <TrackingLink
+        href="/teams/"
         clickType="Card Click"
         clickName="Get Started SigNoz"
         clickLocation="Get Started SigNoz Card"
         clickText="Start Your Free Trial"
         className="block w-full"
       >
-        <Image 
-          src="/img/launch_week/try-signoz-cloud-blog-cta.png" 
+        <Image
+          src="/img/launch_week/try-signoz-cloud-blog-cta.png"
           alt="Get Started - Free CTA"
           width={0}
           height={0}
           sizes="100vw"
-          className="w-full h-auto"
+          className="h-auto w-full"
           style={{ maxWidth: '100%' }}
         />
       </TrackingLink>
 
       <p>
-        You can also install and self-host SigNoz yourself since it is open-source. With 20,000+ GitHub stars,{' '}
-        <TrackingLink 
-          href="https://github.com/signoz/signoz" 
+        You can also install and self-host SigNoz yourself since it is open-source. With 24,000+
+        GitHub stars,{' '}
+        <TrackingLink
+          href="https://github.com/signoz/signoz"
           clickType="External Click"
           clickName="GitHub Repository Link"
           clickLocation="Get Started SigNoz Card"
           clickText="open-source SigNoz"
-          className="text-blue-600 hover:underline font-medium"
+          className="font-medium text-blue-600 hover:underline"
         >
           open-source SigNoz
         </TrackingLink>{' '}
         is loved by developers. Find the{' '}
-        <TrackingLink 
-          href="/docs/install/" 
+        <TrackingLink
+          href="/docs/install/"
           clickType="Nav Click"
           clickName="Docs Link"
           clickLocation="Get Started SigNoz Card"
           clickText="instructions"
-          className="text-blue-600 hover:underline font-medium"
+          className="font-medium text-blue-600 hover:underline"
         >
           instructions
         </TrackingLink>{' '}
