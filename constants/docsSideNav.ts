41ab2598ac8127c3e39db7d4e5854373a7d01b71
Shubham Dubey <shubham@signoz.io>
2025-12-16 16:53:48 +0530

    feat(traces/python): consolidate Python instrumentation docs
    Signed-off-by: Shubham Dubey <shubham@signoz.io>

--- constants/docsSideNav.ts
+++ constants/docsSideNav.ts
@@ -888,43 +888,11 @@ const docsSideNav = [
             isExpanded: false,
             label: 'Python',
             route: '/docs/instrumentation/opentelemetry-python',
-            // link: {
-            // type: 'doc',
-            // route: '/docs/instrumentation/opentelemetry-python',
-            // title: 'Python',
-            // description: 'Learn how to instrument Python applications with OpenTelemetry',
-            // slug: 'instrumentation/python',
-            // },
             items: [
               {
                 type: 'doc',
-                route: '/docs/instrumentation/opentelemetry-django',
-                label: 'Django ',
-              },
-              {
-                type: 'doc',
-                route: '/docs/instrumentation/opentelemetry-fastapi',
-                label: 'FastAPI ',
-              },
-              {
-                type: 'doc',
-                route: '/docs/instrumentation/opentelemetry-flask',
-                label: 'Flask ',
-              },
-              {
-                type: 'doc',
-                route: '/docs/instrumentation/opentelemetry-falcon',
-                label: 'Falcon ',
-              },
-              {
-                type: 'doc',
-                route: '/docs/instrumentation/opentelemetry-hypercorn-unicorn-support',
-                label: 'Hypercorn/Unicorn ',
-              },
-              {
-                type: 'doc',
-                route: '/docs/instrumentation/opentelemetry-celery',
-                label: 'Celery Worker OpenTelemetry Setup',
+                route: '/docs/instrumentation/manual-instrumentation/python/manual-instrumentation',
+                label: 'Manual Instrumentation',
               },
             ],
           },
@@ -1100,6 +1068,19 @@ const docsSideNav = [
                     label: 'Enable/disable Instrumentation',
                     route:
                       '/docs/instrumentation/manual-instrumentation/javascript/nodejs-selective-instrumentation',
+                  },
+                ],
+              },
+              {
+                type: 'category',
+                isExpanded: false,
+                label: 'Python',
+                items: [
+                  {
+                    type: 'doc',
+                    label: 'Manual Instrumentation',
+                    route:
+                      '/docs/instrumentation/manual-instrumentation/python/manual-instrumentation',
                   },
                 ],
               },
