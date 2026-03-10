d4cbeea3f050e652a262d25e3f2ee065f5023551
manika-signoz <manika@signoz.io>
2026-03-10 18:54:45 +0530

    init: clickstack alternative page

--- shared/components/molecules/FeaturePages/FeatureCard/FeatureCard.types.ts
+++ shared/components/molecules/FeaturePages/FeatureCard/FeatureCard.types.ts
@@ -1,7 +1,7 @@
 export interface FeatureCardProps {
   icon: React.ReactNode
   title: string
-  description: string
+  description: string | React.ReactNode
   variant?: 'default' | 'combined'
   className?: string
 }
