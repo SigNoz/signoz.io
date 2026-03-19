e1e2db20f5e5804d060ae0c17e0e2126a834b179
manika-signoz <manika@signoz.io>
2026-03-19 14:04:46 +0530

    revert card changes

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -9,7 +9,7 @@ export const cardVariants = cva('relative', {
       gradient: 'shadow-[-4px_0_20px_rgba(78,116,248,0.3),5px_0_20px_rgba(229,72,77,0.2)]',
       amber: 'shadow-[0_0_40px_rgba(255,205,86,0.3),0_0_80px_rgba(213,170,69,0.2)]',
       primary: 'shadow-[0_0_40px_rgba(78,116,248,0.3),0_0_80px_rgba(63,94,204,0.2)]',
-      cherry: 'shadow-[0_0_10px_rgba(229,72,77,0.3),0_0_10px_rgba(184,58,62,0.2)]',
+      cherry: 'shadow-[0_0_40px_rgba(229,72,77,0.3),0_0_80px_rgba(184,58,62,0.2)]',
       aqua: 'shadow-[0_0_20px_rgba(78,116,248,0.2)]',
       sakura: 'shadow-[0_0_40px_rgba(242,71,105,0.3),0_0_80px_rgba(197,57,85,0.2)]',
       forest: 'shadow-[0_0_40px_rgba(37,225,146,0.3),0_0_80px_rgba(30,180,117,0.2)]',
