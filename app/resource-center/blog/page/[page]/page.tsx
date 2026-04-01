14beb747d67a67a16952d9e94412b3ae6e7e018d
manika-signoz <manika@signoz.io>
2026-04-01 17:06:26 +0530

    chore: fix duplicate title and meta description

--- app/resource-center/blog/page/[page]/page.tsx
+++ app/resource-center/blog/page/[page]/page.tsx
@@ -2,6 +2,37 @@ import { allCoreContent, sortPosts } from 'pliny/utils/contentlayer'
 import GridLayout from '@/layouts/GridLayout'
 import { allBlogs } from 'contentlayer/generated'
 import React from 'react'
+import { genPageMetadata } from 'app/seo'
+import siteMetadata from '@/data/siteMetadata'
+import { Metadata } from 'next'
+
+export async function generateMetadata({ params }: { params: { page: string } }) {
+  return {
+    title: `Blog - Page ${params.page}`,
+    description: `SigNoz is an open-source observability tool powered by OpenTelemetry. Get APM, logs, traces, metrics, exceptions, & alerts in a single tool. | Blog - Page ${params.page} | SigNoz`,
+    openGraph: {
+      title: `Blog - Page ${params.page} | SigNoz`,
+      description: `SigNoz is an open-source observability tool powered by OpenTelemetry. Get APM, logs, traces, metrics, exceptions, & alerts in a single tool. | Blog - Page ${params.page} | SigNoz`,
+      url: `${siteMetadata.siteUrl}/resource-center/blog/page/${params.page}`,
+      siteName: siteMetadata.title,
+      locale: 'en_US',
+      type: 'website',
+      images: [siteMetadata.socialBanner],
+    },
+    twitter: {
+      title: `Blog - Page ${params.page}`,
+      description: `SigNoz is an open-source observability tool powered by OpenTelemetry. Get APM, logs, traces, metrics, exceptions, & alerts in a single tool. | Blog - Page ${params.page} | SigNoz`,
+      images: [siteMetadata.socialBanner],
+    },
+    alternates: {
+      canonical: `${siteMetadata.siteUrl}/resource-center/blog/page/${params.page}`,
+    },
+    robots: {
+      index: false,
+      follow: true,
+    },
+  }
+}
 
 const POSTS_PER_PAGE = 10
 
@@ -31,7 +62,7 @@ export default function Page({ params }: { params: { page: string } }) {
       initialDisplayPosts={initialDisplayPosts}
       pagination={pagination}
       title="All Posts"
-      isDarkMode = {true}
+      isDarkMode={true}
     />
   )
 }
