e2cd73b7d6649a8532f9d32562aac777c0226c31
manika-signoz <manika@signoz.io>
2026-04-02 11:24:17 +0530

    test

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -87,6 +87,7 @@ import DatabaseTable from './HighCardinalityData/DatabaseTable'
 import MCPInstallButton from './MCPInstallButton/MCPInstallButton'
 import dynamic from 'next/dynamic'
 const IncidentCostGraphic = dynamic(() => import('./Blog/IncidentCostGraphic'), { ssr: false })
+import KeyPointCalloutWrapper from './KeyPointCalloutWrapper'
 
 export const components: MDXComponents = {
   Region,
@@ -126,15 +127,7 @@ export const components: MDXComponents = {
   GrafanaVsSigNoz,
   NewRelicVsSigNoz,
   DatadogAlternativesFinder,
-  KeyPointCallout: (props) => {
-    const title = props.title || 'Note'
-    const type = props.type || 'info'
-    return (
-      <Admonition title={title} type={type} {...props}>
-        {props.children}
-      </Admonition>
-    )
-  },
+  KeyPointCallout: KeyPointCalloutWrapper,
   GetStartedOpenTelemetryButton,
   InterlinkCard,
   InArticleVideoShowcaseModal,
