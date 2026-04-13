00af75744b543ad3dc67f84734c3d3ba881fb7fe
Luffy <55076843+LuffySama-Dev@users.noreply.github.com>
2026-04-13 10:40:05 +0530

    Apply suggestion from @github-actions[bot]
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- app/application-performance-monitoring/page.tsx
+++ app/application-performance-monitoring/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: {
-    absolute: 'APM | SigNoz',
+    absolute: 'Application Performance Monitoring (APM) with OpenTelemetry | SigNoz',
   },
   openGraph: {
     title: 'Application Performance Monitoring (APM) with OpenTelemetry | SigNoz',
