9bb27fd97aa6e10098d18ddffe0b552392e41f86
Goutham Karthi <goutham@signoz.io>
2026-04-16 11:29:12 -0700

    docs: post deploy monitoring mcp sue case

--- constants/docsSideNav.ts
+++ constants/docsSideNav.ts
@@ -2990,6 +2990,11 @@ const docsSideNav = [
             route: '/docs/ai/use-cases/alert-correlation-analysis',
             label: 'Alert Correlation Analysis',
           },
+          {
+            type: 'doc',
+            route: '/docs/ai/use-cases/post-deployment-monitoring',
+            label: 'Post Deployment Monitoring',
+          },
         ],
       },
     ],
