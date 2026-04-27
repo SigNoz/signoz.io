0b89a1fa0d8b4357de246ba8efcc8d2dc13c23b4
manika-signoz <manika@signoz.io>
2026-04-27 18:40:52 +0530

    feat: dynamic api reference

--- components/OpenAPISpec/index.tsx
+++ components/OpenAPISpec/index.tsx
@@ -3,8 +3,20 @@
 import './APIReference.styles.css'
 import { API } from '@stoplight/elements'
 import '@stoplight/elements/styles.min.css'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default function OpenAPISpec() {
-  return <API apiDescriptionUrl="/openAPISpec/api.yaml" router="hash" layout="responsive" />
+interface OpenAPISpecProps {
+  specContent: string
+}
+
+export default function OpenAPISpec({ specContent }: OpenAPISpecProps) {
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
+
+  return <API apiDescriptionDocument={specContent} router="hash" layout="responsive" />
 }
