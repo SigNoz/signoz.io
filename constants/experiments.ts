ef6b21c917978ab9e43c64f7b1dfa276499ad529
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2026-04-30 11:48:31 +0530

    chore(teams): mark experiment entry concluded

--- constants/experiments.ts
+++ constants/experiments.ts
@@ -75,6 +75,16 @@ export const EXPERIMENTS = {
     },
     flagName: 'teams-page-focused-layout-experiment',
   },
+  TEAMS_PAGE_VALUE_PROPS: {
+    id: 'teams-page-value-props',
+    variants: {
+      CONTROL: 'current-layout',
+      VARIANT: 'value-props-social-proof',
+    },
+    flagName: 'teams-page-value-props',
+    concluded: true,
+    defaultVariant: 'value-props-social-proof',
+  },
   CLOUD_FIRST_PRICING_PAGE: {
     id: 'cloud-first-pricing-page',
     variants: {
