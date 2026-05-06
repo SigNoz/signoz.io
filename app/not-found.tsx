c97780f9c4c8a1e7362b8ae1aba65fff3c5bb507
Vinícius Lourenço <vinicius@signoz.io>
2026-05-06 15:50:56 -0300

    fix(not-found): missing layout

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,18 @@
 import NotFoundRecoveryClient from '@/components/not-found/NotFoundRecoveryClient'
+import TopNav from '@/components/TopNav/TopNav'
+import SectionContainer from '@/components/SectionContainer'
+import MainFooter from '@/components/mainFooter'
 
 export default function NotFound() {
-  return <NotFoundRecoveryClient />
+  return (
+    <SectionContainer>
+      <div className="relative flex h-screen flex-col justify-between ">
+        <TopNav />
+        <main className="mb-auto mt-[48px] bg-signoz_ink-500">
+          <NotFoundRecoveryClient />
+        </main>
+        <MainFooter />
+      </div>
+    </SectionContainer>
+  )
 }
