844e1a7c378ba28853f6ae9b633c9e883a7ff8dc
Jugal Kishore <jugal@signoz.io>
2026-05-07 17:09:23 +0530

    feat: Add Azure Integration Docs

---
+++ constants/componentItems/azureOneClick.ts
@@ -0,0 +1,14 @@
+import type { ComponentItem } from './types'
+
+export const AZURE_ONE_CLICK_ITEMS = [
+  {
+    name: 'CDN FrontDoor',
+    href: 'https://signoz.io/docs/integrations/azure/cdn-frontdoor/',
+    clickName: 'CDN FrontDoor Integration Link',
+  },
+  {
+    name: 'Blob Storage',
+    href: 'https://signoz.io/docs/integrations/azure/blob-storage/',
+    clickName: 'Blob Storage Integration Link',
+  },
+] satisfies ComponentItem[]
