4c45215486ee944f1abc7420fada14d830ff0f82
swapnil-signoz <swapnil@signoz.io>
2026-06-10 17:05:00 +0530

    feat: adding docs for Azure compute services

--- components/Azure/AzureOneClickListicle.tsx
+++ components/Azure/AzureOneClickListicle.tsx
@@ -19,6 +19,26 @@ const ICON_MAP: Record<string, React.ReactNode> = {
       className="h-7 w-7"
     />
   ),
+  '/docs/integrations/azure/virtual-machines': (
+    <img
+      src="/img/icons/azure-virtual-machines-icon.svg"
+      alt="Azure Virtual Machines"
+      className="h-7 w-7"
+    />
+  ),
+  '/docs/integrations/azure/container-apps': (
+    <img
+      src="/img/icons/azure-container-apps-icon.svg"
+      alt="Azure Container Apps"
+      className="h-7 w-7"
+    />
+  ),
+  '/docs/integrations/azure/app-service': (
+    <img src="/img/icons/azure-app-service-icon.svg" alt="Azure App Services" className="h-7 w-7" />
+  ),
+  '/docs/integrations/azure/aks': (
+    <img src="/img/icons/azure-aks-icon.svg" alt="Azure Kubernetes Service" className="h-7 w-7" />
+  ),
 }
 
 export default function AzureOneClickListicle() {
--- constants/componentItems/azureOneClick.ts
+++ constants/componentItems/azureOneClick.ts
@@ -11,4 +11,24 @@ export const AZURE_ONE_CLICK_ITEMS = [
     href: '/docs/integrations/azure/blob-storage',
     clickName: 'Blob Storage Integration Link',
   },
+  {
+    name: 'Virtual Machines',
+    href: '/docs/integrations/azure/virtual-machines',
+    clickName: 'Virtual Machines Integration Link',
+  },
+  {
+    name: 'Container Apps',
+    href: '/docs/integrations/azure/container-apps',
+    clickName: 'Container Apps Integration Link',
+  },
+  {
+    name: 'App Services',
+    href: '/docs/integrations/azure/app-service',
+    clickName: 'App Services Integration Link',
+  },
+  {
+    name: 'AKS',
+    href: '/docs/integrations/azure/aks',
+    clickName: 'AKS Integration Link',
+  },
 ] satisfies ComponentItem[]
