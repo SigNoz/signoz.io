3fa22a120d6368d50cc5bb86b114d5866f815ecf
Dhruv <dhruvahuja2k@gmail.com>
2026-06-23 15:30:54 +0530

    blog: Update Comparison Articles (#3544)
    * update new relic article
    
    * update open source dd article
    
    * fix typo in newrelic article
    
    * update otel alternatives blog
    
    * update aws alternatives
    
    * update axiom alternatives
    
    * update azure alternatives
    
    * update checkmk article
    
    * update clickhouse alternatives
    
    * update cloudwatch, datadog alternatives
    
    * update DO, docker alternatives
    
    * update dynatrace, ELK articles
    
    * remove elk history paragraph
    
    * update firebase alternatives
    
    * update grafana, frontend cloud articles
    
    * update graylog, heroku articles
    
    * update inciga blog
    
    * undo fix stale URLs
    
    * Delete components/shared/k8s-next-steps.md
    
    * Add callout for AWS ecosystem considerations
    
    * wrap links in callout
    
    * fix deadlinks
    
    * update content flow

--- components/shared/K8sNextSteps.tsx
+++ components/shared/K8sNextSteps.tsx
@@ -4,12 +4,12 @@ export default function K8sNextSteps() {
   return (
     <ul>
       <li>
-        <CustomLink href="https://signoz.io/docs/collection-agents/k8s/k8s-infra/overview/">
+        <CustomLink href="https://signoz.io/docs/opentelemetry-collection-agents/k8s/k8s-infra/overview/">
           Collect Telemetry from your K8s Clusters
         </CustomLink>
       </li>
       <li>
-        <CustomLink href="https://signoz.io/docs/tutorial/opentelemetry-operator-usage/#opentelemetry-auto-instrumentation-injection">
+        <CustomLink href="https://signoz.io/docs/opentelemetry-collection-agents/k8s/otel-operator/overview/">
           Use OpenTelemetry Operator for automatic instrumentation
         </CustomLink>
       </li>
