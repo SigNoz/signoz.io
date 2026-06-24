598834843dfdef1f331c09523bafed9ca6b387e3
Manika Malhotra <manika@signoz.io>
2026-06-24 13:51:34 +0530

    fix(librarytabs): unify tabs and library tabs, add variants, update mdx files to use tabs

---
+++ components/TabItem.tsx
@@ -0,0 +1,12 @@
+export interface TabItemProps {
+  value: string
+  label: React.ReactNode
+  default?: boolean
+  children?: React.ReactNode
+}
+
+const TabItem = ({ children }: TabItemProps) => {
+  return <>{children}</>
+}
+
+export default TabItem
