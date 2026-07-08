c603ae8347acb02368d837f849ba73d713bec9e3
Manika Malhotra <manika@signoz.io>
2026-07-08 18:32:29 +0530

    feat: kubernetes monitoring page (#3662)
    * feat: kubernetes monitoring page
    
    * chore: add read more cta to pricing and resource scaling sections
    
    * chore: change noz k8s chat image
    
    * chore: update meta title to match h1
    
    * chore: resolve comments
    
    * chore: update clicknames
    
    * revert: accidental change
    
    * feat: add titleLevel to iconcardgrid component and use h2 in k8s monitoring page
    
    * chore: add page to products sitemap
    
    * chore: link kubernetes monitoring to use cases dropdown

--- shared/components/molecules/FeaturePages/IconTitleDescriptionCard/IconTitleDescriptionCardGrid.view.tsx
+++ shared/components/molecules/FeaturePages/IconTitleDescriptionCard/IconTitleDescriptionCardGrid.view.tsx
@@ -7,6 +7,7 @@ const IconTitleDescriptionCardGrid: React.FC<IconTitleDescriptionCardGridProps> 
   cards,
   className = '',
   variant = 'lg',
+  titleLevel,
 }) => {
   return (
     <div className={cn('grid grid-cols-1 md:grid-cols-2', className)}>
@@ -25,6 +26,7 @@ const IconTitleDescriptionCardGrid: React.FC<IconTitleDescriptionCardGridProps> 
               description={card.description}
               className={card.className}
               variant={variant}
+              titleLevel={titleLevel}
             />
             {!isLast && <Divider className="absolute bottom-0 left-0 md:hidden" />}
             {!isLastDesktopRow && <Divider className="absolute bottom-0 left-0 hidden md:block" />}
