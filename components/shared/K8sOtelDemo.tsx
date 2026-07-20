a172c6d328d96f2f05119b242b6a631861ed86c4
Manika Malhotra <manika@signoz.io>
2026-07-20 20:31:06 +0530

    chore: migrate K8sOtelDemo to signozhq

--- components/shared/K8sOtelDemo.tsx
+++ components/shared/K8sOtelDemo.tsx
@@ -1,36 +1,38 @@
+import { Typography } from '@signozhq/ui/typography'
 import { RegionAwareCode, RegionAwarePre } from '@/components/Region/RegionAwareComponents'
 
 export default function K8sOtelDemo() {
   return (
     <>
-      <p>
+      <Typography.Text as="p">
         The OpenTelemetry Demo is a microservice-based distributed system intended to illustrate the
         implementation of OpenTelemetry in a near real-world environment. See more details at{' '}
         <a
           href="https://opentelemetry.io/ecosystem/demo/"
           target="_blank"
           rel="noopener noreferrer nofollow"
+          className="text-[var(--accent-primary)] hover:underline"
         >
           OpenTelemetry Demo
         </a>
         .
-      </p>
+      </Typography.Text>
       <ol>
         <li>
-          <p>Get the address of the SigNoz collector:</p>
+          <Typography.Text as="p">Get the address of the SigNoz collector:</Typography.Text>
           <RegionAwarePre>
             <RegionAwareCode className="language-bash">{`kubectl get -n <namespace> svc/signoz-otel-collector`}</RegionAwareCode>
           </RegionAwarePre>
-          <p>
+          <Typography.Text as="p">
             This value will be used in the next step to configure the OpenTelemetry Demo to send
             data to the SigNoz collector.
-          </p>
+          </Typography.Text>
         </li>
         <li>
-          <p>
+          <Typography.Text as="p">
             Create a <code>values.yaml</code> file that will contain the configuration for the chart
             and send it to your SigNoz installation:
-          </p>
+          </Typography.Text>
           <RegionAwarePre>
             <RegionAwareCode className="language-yaml">{`default:
   env:
@@ -46,28 +48,29 @@ export default function K8sOtelDemo() {
     - name: OTEL_COLLECTOR_NAME
       value: signoz-otel-collector.<namespace>.svc.cluster.local`}</RegionAwareCode>
           </RegionAwarePre>
-          <p>
+          <Typography.Text as="p">
             Note: The <code>OTEL_COLLECTOR_NAME</code> is the address obtained in the previous step.
-          </p>
+          </Typography.Text>
         </li>
         <li>
-          <p>Install OpenTelemetry Demo:</p>
+          <Typography.Text as="p">Install OpenTelemetry Demo:</Typography.Text>
           <RegionAwarePre>
             <RegionAwareCode className="language-bash">{`helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo update
 helm install otel-demo open-telemetry/opentelemetry-demo -f values.yaml`}</RegionAwareCode>
           </RegionAwarePre>
-          <p>
+          <Typography.Text as="p">
             More details on the installation can be found{' '}
             <a
               href="https://opentelemetry.io/docs/demo/kubernetes-deployment/"
               target="_blank"
               rel="noopener noreferrer nofollow"
+              className="text-[var(--accent-primary)] hover:underline"
             >
               here
             </a>
             .
-          </p>
+          </Typography.Text>
         </li>
       </ol>
     </>
