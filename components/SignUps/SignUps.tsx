ec6c5c779bb2c297d45d9996eb4dcda91c9076c8
Manika Malhotra <manika@signoz.io>
2026-07-20 20:35:01 +0530

    chore: migrate SignUps to signozhq

--- components/SignUps/SignUps.tsx
+++ components/SignUps/SignUps.tsx
@@ -4,5 +4,9 @@ import React from 'react'
 const markdownContent = `[![Pricing Page](/img/blog/common/signup_cta_1.webp)](https://signoz.io/pricing/)`
 
 export default function SignUps() {
-  return <MarkdownRenderer markdownContent={markdownContent} />
+  return (
+    <div className="overflow-hidden rounded-md border border-[var(--l2-border)] bg-[var(--l2-background)]">
+      <MarkdownRenderer markdownContent={markdownContent} />
+    </div>
+  )
 }
