7ea51a34a5770109771c4de5b349ce59a72de22d
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2026-07-27 14:35:59 +0530

    Replace Parallel logos with Hedra (#3827)
    * Replace Parallel logos with Hedra
    
    * Address Hedra logo review feedback
    
    * Balance Hedra logo sizing
    
    * Use official Hedra logo lockup

--- components/index-header/homepage-customer-proof/CustomerProofBoard.tsx
+++ components/index-header/homepage-customer-proof/CustomerProofBoard.tsx
@@ -47,7 +47,7 @@ export const BentoBoard = memo(function BentoBoard({ isClone }: { isClone: boole
         />
       </BentoCell>
       <BentoCell column={7} columnSpan={2} row={1}>
-        <LogoCard isClone={isClone} logo={logos.parallel} />
+        <LogoCard isClone={isClone} logo={logos.hedra} />
       </BentoCell>
       <BentoCell column={9} columnSpan={4} row={1}>
         <QuoteCard
