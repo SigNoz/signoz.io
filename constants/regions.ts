1bffbdd859ff5f0c219752edc4d001e0a1fb316d
Manika Malhotra <manika@signoz.io>
2026-08-06 19:47:12 +0530

    feat: update the signup flow for verification ttl (#3903)
    * feat: update the signup flow for verification ttl
    
    * fix: minor copy fixes and copy link email params bug
    
    * revert: url encode fix, not in scope

--- constants/regions.ts
+++ constants/regions.ts
@@ -5,7 +5,7 @@ export interface Region {
 }
 
 export const REGIONS: Region[] = [
-  { name: 'United States', id: 'us', iconURL: '/svgs/icons/us.svg' },
-  { name: 'Europe', id: 'eu', iconURL: '/svgs/icons/eu.svg' },
-  { name: 'India', id: 'in', iconURL: '/svgs/icons/india.svg' },
+  { name: 'United States', id: 'us2', iconURL: '/svgs/icons/us.svg' },
+  { name: 'Europe', id: 'eu2', iconURL: '/svgs/icons/eu.svg' },
+  { name: 'India', id: 'in2', iconURL: '/svgs/icons/india.svg' },
 ]
