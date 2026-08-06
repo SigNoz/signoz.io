98dcb279209016c9676fdf395f5ded81247178f7
Yuvraj Singh Jadon <yuvraajsj18@gmail.com>
2026-08-06 15:21:14 +0530

    Remove homepage self-host inline link

--- components/index-features/index.tsx
+++ components/index-features/index.tsx
@@ -16,7 +16,6 @@ import featureGraphic5 from '@/public/img/graphics/homepage/feature-graphic-5.sv
 import featureGraphic6 from '@/public/img/graphics/homepage/feature-graphic-6.svg?url'
 import featureGraphic8 from '@/public/img/graphics/homepage/feature-graphic-8.svg?url'
 import { cn } from '../../app/lib/utils'
-import TrackingLink from '@/components/TrackingLink'
 
 const icons = [
   {
@@ -148,18 +147,7 @@ export const SigNozFeatures = ({ className }: { className?: string }) => {
                 SigNoz Cloud is a managed Datadog or New Relic alternative for logs, metrics,
                 traces, dashboards, alerts, and more.
                 <br />
-                Run it on your own infrastructure with{' '}
-                <TrackingLink
-                  className="underline underline-offset-4 transition-colors hover:text-signoz_sienna-100"
-                  clickLocation="Homepage Features Intro"
-                  clickName="Install Link"
-                  clickText="Install Self-Hosted SigNoz"
-                  clickType="Inline Link"
-                  href="/docs/install/"
-                >
-                  Self-Hosted SigNoz
-                </TrackingLink>
-                .
+                Run it on your own infrastructure with Self-Hosted SigNoz.
               </p>
             </div>
 
