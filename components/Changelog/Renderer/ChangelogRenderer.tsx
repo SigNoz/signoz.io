2cda09a6105e074fb50523c016bded1b3d4042e5
Manika Malhotra <manika@signoz.io>
2026-08-14 11:38:26 +0530

    feat(changelog): make links open in new tab

--- components/Changelog/Renderer/ChangelogRenderer.tsx
+++ components/Changelog/Renderer/ChangelogRenderer.tsx
@@ -10,8 +10,23 @@ import { SupportedImageTypes, SupportedVideoTypes } from '@/utils/strapi'
 import ChangelogTitle from '@/components/Changelog/Title/ChangelogTitle'
 import Link from 'next/link'
 
+function MarkdownLink({ href, children, ...props }: React.AnchorHTMLAttributes<HTMLAnchorElement>) {
+  const isSignozUrl =
+    /^(https?:)?\/\/([^/]*\.)?signoz\.io/i.test(href ?? '') || Boolean(href?.startsWith('/'))
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel={isSignozUrl ? 'noopener' : 'noopener noreferrer nofollow'}
+      {...props}
+    >
+      {children}
+    </a>
+  )
+}
+
 function renderMarkdown(markdownContent: string) {
-  return <ReactMarkdown>{markdownContent}</ReactMarkdown>
+  return <ReactMarkdown components={{ a: MarkdownLink }}>{markdownContent}</ReactMarkdown>
 }
 
 function renderMedia(media: Media) {
